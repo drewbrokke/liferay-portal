1b265f11445d4e94eee76e39630ca20ea330ff36
Marc Lundgren <marc.lundgren@liferay.com>
2014-06-12 10:01:18 -0700

    LPS-37751 - make NAMES_HASH object-oriented

--- portal-web/docroot/html/portlet/dynamic_data_mapping/js/main.js
+++ portal-web/docroot/html/portlet/dynamic_data_mapping/js/main.js
@@ -166,7 +166,18 @@ AUI.add(
 				NAME: 'liferayformbuilder',
 
 				prototype: {
-					NAMES_HASH: {},
+					NAMES: {
+						hash: {},
+						add: function(value) {
+							this.hash[value] = true;
+						},
+						get: function(value) {
+							return this.hash[value];
+						},
+						remove: function(value) {
+							delete this.hash[value];
+						}
+					},
 
 					initializer: function() {
 						var instance = this;
@@ -204,7 +215,7 @@ AUI.add(
 						field.set('readOnlyAttributes', instance._getReadOnlyFieldAttributes(field));
 						field.set('strings', instance.get('strings'));
 
-						instance.NAMES_HASH[field.get('name')] = true;
+						instance.NAMES.add(field.get('name'));
 
 						return field;
 					},
@@ -520,9 +531,7 @@ AUI.add(
 								}
 							);
 
-							var namesList = instance.NAMES_HASH;
-
-							if (namesList[newValue]) {
+							if (instance.NAMES.get(newValue)) {
 								alert(instance.get('strings').duplicateNameMessage);
 
 								editingField.focus();
@@ -530,9 +539,9 @@ AUI.add(
 
 							else {
 								if (newValue !== undefined) {
-									delete namesList[oldValue];
+									instance.NAMES.remove(oldValue);
 
-									namesList[newValue] = true;
+									instance.NAMES.add(newValue);
 								}
 
 								LiferayFormBuilder.superclass._handleSaveEvent.apply(this, arguments);
