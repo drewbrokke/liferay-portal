23d13b43465a73619184632e0ebd019e8a33262a
Pei-Jung Lan <pei-jung.lan@liferay.com>
2016-02-18 18:34:33 -0800

    LPS-62941 OpenSSO - add user friendly and translatable names and descriptions to the methods

--- modules/portal-security-sso/portal-security-sso-opensso/src/main/java/com/liferay/portal/security/sso/opensso/configuration/OpenSSOConfiguration.java
+++ modules/portal-security-sso/portal-security-sso-opensso/src/main/java/com/liferay/portal/security/sso/opensso/configuration/OpenSSOConfiguration.java
@@ -40,8 +40,7 @@ public interface OpenSSOConfiguration {
 	public boolean enabled();
 
 	@Meta.AD(
-		deflt = "false",
-		description = "Set this to true if you want to import the users from LDAP after successful login through OpenSSO. The LDAP settings need to be configured properly in the LDAP section. If this is set to false, the users will be created from OpenSSO provided data.",
+		deflt = "false", description = "%import-from-ldap-help",
 		required = false
 	)
 	public boolean importFromLDAP();
@@ -53,8 +52,7 @@ public interface OpenSSOConfiguration {
 	public String loginURL();
 
 	@Meta.AD(
-		deflt = "false",
-		description = "Set this to true to log a user out of OpenSSO when the portal session expires.",
+		deflt = "false", description = "%logout-on-session-expiration-help",
 		required = false
 	)
 	public boolean logoutOnSessionExpiration();
@@ -70,16 +68,20 @@ public interface OpenSSOConfiguration {
 	)
 	public String serviceURL();
 
-	@Meta.AD(deflt = "uid", required = false)
+	@Meta.AD(deflt = "uid", name = "%screen-name-attribute", required = false)
 	public String screenNameAttr();
 
-	@Meta.AD(deflt = "mail", required = false)
+	@Meta.AD(
+		deflt = "mail", name = "%email-address-attribute", required = false
+	)
 	public String emailAddressAttr();
 
-	@Meta.AD(deflt = "givenname", required = false)
+	@Meta.AD(
+		deflt = "givenname", name = "%first-name-attribute", required = false
+	)
 	public String firstNameAttr();
 
-	@Meta.AD(deflt = "sn", required = false)
+	@Meta.AD(deflt = "sn", name = "%last-name-attribute", required = false)
 	public String lastNameAttr();
 
 }
