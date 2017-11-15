80203e181423eecc910c5e107ab1a42e13b22c34
Pei-Jung Lan <pei-jung.lan@liferay.com>
2017-11-15 15:47:56 -0800

    LPS-75333 Add ConfigurationRenderCommandUtil to track ConfigurationRenderCommand services

---
+++ modules/apps/foundation/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/util/ConfigurationRenderCommandUtil.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.configuration.admin.web.internal.util;
+
+import com.liferay.configuration.admin.portlet.action.ConfigurationRenderCommand;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ * @author Pei-Jung Lan
+ */
+public class ConfigurationRenderCommandUtil {
+
+	public static ConfigurationRenderCommand getConfigurationRenderCommand(
+		String pid) {
+
+		return _configurationRenderCommandMap.getService(pid);
+	}
+
+	private static final ServiceTrackerMap<String, ConfigurationRenderCommand>
+		_configurationRenderCommandMap;
+
+	static {
+		Bundle bundle = FrameworkUtil.getBundle(
+			ConfigurationRenderCommandUtil.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		_configurationRenderCommandMap =
+			ServiceTrackerMapFactory.openSingleValueMap(
+				bundleContext, ConfigurationRenderCommand.class,
+				"configurationPid");
+	}
+
+}
