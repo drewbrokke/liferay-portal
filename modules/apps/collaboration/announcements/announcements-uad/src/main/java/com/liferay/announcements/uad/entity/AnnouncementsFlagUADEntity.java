2a68a87dc4f78ae2e3e0355d2a6227698ee57b04
Noah Sherrill <noah.sherrill@liferay.com>
2018-01-16 14:10:37 -0800

    LPS-77314 Use ActionableDynamicQueries to execute Announcements bulk operations

--- modules/apps/collaboration/announcements/announcements-uad/src/main/java/com/liferay/announcements/uad/entity/AnnouncementsFlagUADEntity.java
+++ modules/apps/collaboration/announcements/announcements-uad/src/main/java/com/liferay/announcements/uad/entity/AnnouncementsFlagUADEntity.java
@@ -18,10 +18,17 @@ import com.liferay.announcements.kernel.model.AnnouncementsFlag;
 import com.liferay.announcements.uad.constants.AnnouncementsUADConstants;
 import com.liferay.user.associated.data.entity.BaseUADEntity;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Noah Sherrill
  */
 public class AnnouncementsFlagUADEntity extends BaseUADEntity {
+
+	public static List<String> getUserIdFieldNames() {
+		return Arrays.asList("userId");
+	}
 
 	public AnnouncementsFlagUADEntity(
 		long userId, String uadEntityId, AnnouncementsFlag announcementsFlag) {
