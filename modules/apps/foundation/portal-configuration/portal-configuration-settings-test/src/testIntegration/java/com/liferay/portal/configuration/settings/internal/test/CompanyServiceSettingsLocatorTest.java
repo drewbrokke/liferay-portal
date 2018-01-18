8ae1b7a51cfdf7372bc9342fa963856ff66e0a61
drewbrokke <drew.brokke@liferay.com>
2018-01-17 16:43:11 -0800

    LPS-76948 portal-configuration-settings-test - refactor - moves format string to constants file

--- modules/apps/foundation/portal-configuration/portal-configuration-settings-test/src/testIntegration/java/com/liferay/portal/configuration/settings/internal/test/CompanyServiceSettingsLocatorTest.java
+++ modules/apps/foundation/portal-configuration/portal-configuration-settings-test/src/testIntegration/java/com/liferay/portal/configuration/settings/internal/test/CompanyServiceSettingsLocatorTest.java
@@ -75,7 +75,7 @@ public class CompanyServiceSettingsLocatorTest
 				_companyId, _companyId, PortletKeys.PREFS_OWNER_TYPE_COMPANY, 0,
 				_portletId, null,
 				String.format(
-					_portletPreferenceFormat,
+					SettingsLocatorTestConstants.PORTLET_PREFERENCES_FORMAT,
 					SettingsLocatorTestConstants.TEST_KEY,
 					companyPortletPreferencesValue)));
 
@@ -92,10 +92,6 @@ public class CompanyServiceSettingsLocatorTest
 
 		return settings.getValue(SettingsLocatorTestConstants.TEST_KEY, null);
 	}
-
-	private static final String _portletPreferenceFormat =
-		"<portlet-preferences><preference><name>%s</name><value>%s</value>" +
-			"</preference></portlet-preferences>";
 
 	private long _companyId;
 	private String _portletId;
