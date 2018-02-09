95e6e2d04996838ef12d2d54c5e81b96d9cb3ea7
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-02-08 18:26:33 -0800

    LPS-77317 Move search container logic to display class and simplify

--- modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/display/ManageUserAssociatedDataEntitiesDisplay.java
+++ modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/display/ManageUserAssociatedDataEntitiesDisplay.java
@@ -15,8 +15,16 @@
 package com.liferay.user.associated.data.web.internal.display;
 
 import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.portlet.PortletURLUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.user.associated.data.aggregator.UADEntityAggregator;
 import com.liferay.user.associated.data.display.UADEntityDisplay;
 import com.liferay.user.associated.data.entity.UADEntity;
+import com.liferay.user.associated.data.registry.UADRegistryUtil;
+
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 /**
  * @author Drew Brokke
@@ -24,22 +32,43 @@ import com.liferay.user.associated.data.entity.UADEntity;
 public class ManageUserAssociatedDataEntitiesDisplay {
 
 	public ManageUserAssociatedDataEntitiesDisplay(
-		UADEntityDisplay uadEntityDisplay,
-		SearchContainer<UADEntity> uadEntitySearchContainer) {
+		RenderRequest renderRequest, RenderResponse renderResponse) {
 
-		_uadEntityDisplay = uadEntityDisplay;
-		_uadEntitySearchContainer = uadEntitySearchContainer;
+		_renderRequest = renderRequest;
+		_renderResponse = renderResponse;
 	}
 
 	public UADEntityDisplay getUADEntityDisplay() {
-		return _uadEntityDisplay;
+		String uadRegistryKey = ParamUtil.getString(
+			_renderRequest, "uadRegistryKey");
+
+		return UADRegistryUtil.getUADEntityDisplay(uadRegistryKey);
 	}
 
 	public SearchContainer<UADEntity> getUADEntitySearchContainer() {
-		return _uadEntitySearchContainer;
+		long selUserId = ParamUtil.getLong(_renderRequest, "selUserId");
+		String uadRegistryKey = ParamUtil.getString(
+			_renderRequest, "uadRegistryKey");
+
+		PortletURL iteratorURL = PortletURLUtil.getCurrent(
+			_renderRequest, _renderResponse);
+
+		SearchContainer<UADEntity> searchContainer = new SearchContainer<>(
+			_renderRequest, iteratorURL, null, null);
+
+		UADEntityAggregator uadEntityAggregator =
+			UADRegistryUtil.getUADEntityAggregator(uadRegistryKey);
+
+		searchContainer.setResults(
+			uadEntityAggregator.getUADEntities(
+				selUserId, searchContainer.getStart(),
+				searchContainer.getEnd()));
+		searchContainer.setTotal(uadEntityAggregator.count(selUserId));
+
+		return searchContainer;
 	}
 
-	private final UADEntityDisplay _uadEntityDisplay;
-	private final SearchContainer<UADEntity> _uadEntitySearchContainer;
+	private final RenderRequest _renderRequest;
+	private final RenderResponse _renderResponse;
 
 }
