15a2da77030c99d58871492c3d7b78975257eae9
drewbrokke <drew.brokke@liferay.com>
2019-04-01 15:18:25 -0700

    LPS-90255 portal-settings-web - creates "appearance" ConfigurationScreen entry

---
+++ modules/apps/portal-settings/portal-settings-web/src/main/java/com/liferay/portal/settings/web/internal/portlet/action/AppearancePortalSettingsConfigurationScreenContributor.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.settings.web.internal.portlet.action;
+
+import com.liferay.portal.settings.portlet.action.PortalSettingsConfigurationScreenContributor;
+
+import java.util.Locale;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(service = PortalSettingsConfigurationScreenContributor.class)
+public class AppearancePortalSettingsConfigurationScreenContributor
+	implements PortalSettingsConfigurationScreenContributor {
+
+	@Override
+	public String getCategoryKey() {
+		return "portal-settings";
+	}
+
+	@Override
+	public String getJspPath() {
+		return "/appearance.jsp";
+	}
+
+	@Override
+	public String getKey() {
+		return "appearance";
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		return "appearance";
+	}
+
+	@Override
+	public String getSaveMVCActionCommandName() {
+		return "/portal_settings/edit_company";
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.portal.settings.web)",
+		unbind = "-"
+	)
+	private ServletContext _servletContext;
+
+}
