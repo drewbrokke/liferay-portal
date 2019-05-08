dfb2f507cd57b182b2196e75580ebe91b1c409fe
drewbrokke <drew.brokke@liferay.com>
2019-05-08 12:22:53 -0700

    LPS-95240 - Revert "LPS-95083 Inline"
    This reverts commit 1f4bebbc16b9adc5bad1f0189108f188377d3f2a.

--- modules/apps/portal-configuration/portal-configuration-extender/src/main/java/com/liferay/portal/configuration/extender/internal/ConfiguratorExtender.java
+++ modules/apps/portal-configuration/portal-configuration-extender/src/main/java/com/liferay/portal/configuration/extender/internal/ConfiguratorExtender.java
@@ -15,11 +15,7 @@
 package com.liferay.portal.configuration.extender.internal;
 
 import com.liferay.petra.function.UnsafeFunction;
-import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.PropertiesUtil;
 
 import java.io.IOException;
@@ -37,8 +33,6 @@ import org.apache.felix.cm.file.ConfigurationHandler;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleEvent;
-import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -52,10 +46,13 @@ import org.osgi.util.tracker.BundleTrackerCustomizer;
  * @author Miguel Pastor
  */
 @Component(immediate = true, service = {})
-public class ConfiguratorExtender implements BundleTrackerCustomizer<Bundle> {
+public class ConfiguratorExtender
+	implements BundleTrackerCustomizer<ConfiguratorExtension> {
 
 	@Override
-	public Bundle addingBundle(Bundle bundle, BundleEvent bundleEvent) {
+	public ConfiguratorExtension addingBundle(
+		Bundle bundle, BundleEvent bundleEvent) {
+
 		Dictionary<String, String> headers = bundle.getHeaders(
 			StringPool.BLANK);
 
@@ -81,30 +78,27 @@ public class ConfiguratorExtender implements BundleTrackerCustomizer<Bundle> {
 			return null;
 		}
 
-		String symbolicName = bundle.getSymbolicName();
+		ConfiguratorExtension configuratorExtension = new ConfiguratorExtension(
+			_configurationAdmin, bundle.getSymbolicName(),
+			namedConfigurationContents);
 
-		for (NamedConfigurationContent namedConfigurationContent :
-				namedConfigurationContents) {
+		configuratorExtension.start();
 
-			try {
-				_process(namedConfigurationContent, symbolicName);
-			}
-			catch (Exception e) {
-				_log.error(e, e);
-			}
-		}
-
-		return bundle;
+		return configuratorExtension;
 	}
 
 	@Override
 	public void modifiedBundle(
-		Bundle bundle, BundleEvent bundleEvent, Bundle trackedBundle) {
+		Bundle bundle, BundleEvent bundleEvent,
+		ConfiguratorExtension configuratorExtension) {
 	}
 
 	@Override
 	public void removedBundle(
-		Bundle bundle, BundleEvent bundleEvent, Bundle trackedBundle) {
+		Bundle bundle, BundleEvent bundleEvent,
+		ConfiguratorExtension configuratorExtension) {
+
+		configuratorExtension.destroy();
 	}
 
 	@Activate
@@ -170,69 +164,6 @@ public class ConfiguratorExtender implements BundleTrackerCustomizer<Bundle> {
 					}));
 		}
 	}
-
-	private void _process(
-			NamedConfigurationContent namedConfigurationContent,
-			String symbolicName)
-		throws InvalidSyntaxException, IOException {
-
-		Configuration configuration = null;
-		String configuratorURL = null;
-
-		if (namedConfigurationContent.getFactoryPid() == null) {
-			String pid = namedConfigurationContent.getPid();
-
-			if (ArrayUtil.isNotEmpty(
-					_configurationAdmin.listConfigurations(
-						"(service.pid=" + pid + ")"))) {
-
-				return;
-			}
-
-			configuration = _configurationAdmin.getConfiguration(
-				pid, StringPool.QUESTION);
-		}
-		else {
-			configuratorURL =
-				symbolicName + "#" + namedConfigurationContent.getPid();
-
-			if (ArrayUtil.isNotEmpty(
-					_configurationAdmin.listConfigurations(
-						"(configurator.url=" + configuratorURL + ")"))) {
-
-				return;
-			}
-
-			configuration = _configurationAdmin.createFactoryConfiguration(
-				namedConfigurationContent.getFactoryPid(), StringPool.QUESTION);
-		}
-
-		Dictionary<String, Object> properties = null;
-
-		try {
-			properties = namedConfigurationContent.getProperties();
-		}
-		catch (Throwable t) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					StringBundler.concat(
-						"Supplier from description ", namedConfigurationContent,
-						" threw an exception: "),
-					t);
-			}
-
-			return;
-		}
-
-		if (configuratorURL != null) {
-			properties.put("configurator.url", configuratorURL);
-		}
-
-		configuration.update(properties);
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		ConfiguratorExtender.class);
 
 	private BundleTracker<?> _bundleTracker;
 
