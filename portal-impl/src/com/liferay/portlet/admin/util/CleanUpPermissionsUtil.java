b0234b049861fac366c1bee7387e57c8bd54e96b
drewbrokke <drew.brokke@liferay.com>
2019-06-05 12:14:40 -0700

    LPS-95705 portal-impl - updates usages of RoleConstants

--- portal-impl/src/com/liferay/portlet/admin/util/CleanUpPermissionsUtil.java
+++ portal-impl/src/com/liferay/portlet/admin/util/CleanUpPermissionsUtil.java
@@ -19,7 +19,7 @@ import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.ResourcePermission;
 import com.liferay.portal.kernel.model.Role;
-import com.liferay.portal.kernel.model.RoleConstants;
+import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalServiceUtil;
