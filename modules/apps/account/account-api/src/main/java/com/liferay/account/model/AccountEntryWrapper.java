749ce6698643d91229fa0cecb76cb78802dbbfe5
pei-jung <pei-jung.lan@liferay.com>
2019-10-02 11:48:03 -0700

    LPS-101274 Regen

--- modules/apps/account/account-api/src/main/java/com/liferay/account/model/AccountEntryWrapper.java
+++ modules/apps/account/account-api/src/main/java/com/liferay/account/model/AccountEntryWrapper.java
@@ -52,6 +52,7 @@ public class AccountEntryWrapper
 		attributes.put("parentAccountEntryId", getParentAccountEntryId());
 		attributes.put("name", getName());
 		attributes.put("description", getDescription());
+		attributes.put("domains", getDomains());
 		attributes.put("logoId", getLogoId());
 		attributes.put("status", getStatus());
 
@@ -121,6 +122,12 @@ public class AccountEntryWrapper
 			setDescription(description);
 		}
 
+		String domains = (String)attributes.get("domains");
+
+		if (domains != null) {
+			setDomains(domains);
+		}
+
 		Long logoId = (Long)attributes.get("logoId");
 
 		if (logoId != null) {
@@ -172,6 +179,16 @@ public class AccountEntryWrapper
 	@Override
 	public String getDescription() {
 		return model.getDescription();
+	}
+
+	/**
+	 * Returns the domains of this account entry.
+	 *
+	 * @return the domains of this account entry
+	 */
+	@Override
+	public String getDomains() {
+		return model.getDomains();
 	}
 
 	/**
@@ -322,6 +339,16 @@ public class AccountEntryWrapper
 	@Override
 	public void setDescription(String description) {
 		model.setDescription(description);
+	}
+
+	/**
+	 * Sets the domains of this account entry.
+	 *
+	 * @param domains the domains of this account entry
+	 */
+	@Override
+	public void setDomains(String domains) {
+		model.setDomains(domains);
 	}
 
 	/**
