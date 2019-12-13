6f302a22d8dc4e6ed6fe159517e5b4bb3fc42695
Brian Chan <brian.chan@liferay.com>
2019-12-13 11:56:56 -0800

    LPS-105615 regen

--- modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/graphql/mutation/v1_0/Mutation.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/graphql/mutation/v1_0/Mutation.java
@@ -54,7 +54,7 @@ public class Mutation {
 			subscriptionResourceComponentServiceObjects;
 	}
 
-	@GraphQLField(description = "Deletes an organization")
+	@GraphQLField(description = "Deletes an organization.")
 	public boolean deleteOrganization(
 			@GraphQLName("organizationId") Long organizationId)
 		throws Exception {
