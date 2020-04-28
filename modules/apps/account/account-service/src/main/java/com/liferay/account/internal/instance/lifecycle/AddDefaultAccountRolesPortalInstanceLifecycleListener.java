f1d4159806ed9f01c5d9eb44ef89e18636868b7f
pei-jung <pei-jung.lan@liferay.com>
2020-04-27 20:10:39 -0700

    LPS-108187 Add default permissions to the Account Manager role

--- modules/apps/account/account-service/src/main/java/com/liferay/account/internal/instance/lifecycle/AddDefaultAccountRolesPortalInstanceLifecycleListener.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/internal/instance/lifecycle/AddDefaultAccountRolesPortalInstanceLifecycleListener.java
@@ -16,6 +16,7 @@ package com.liferay.account.internal.instance.lifecycle;
 
 import com.liferay.account.constants.AccountActionKeys;
 import com.liferay.account.constants.AccountConstants;
+import com.liferay.account.constants.AccountPortletKeys;
 import com.liferay.account.constants.AccountRoleConstants;
 import com.liferay.account.model.AccountEntry;
 import com.liferay.account.model.AccountRole;
@@ -25,6 +26,7 @@ import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener
 import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.model.Release;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.Role;
@@ -81,12 +83,20 @@ public class AddDefaultAccountRolesPortalInstanceLifecycleListener
 		}
 
 		if (!_exists(AccountRoleConstants.REQUIRED_ROLE_NAME_ACCOUNT_MANAGER)) {
-			_roleLocalService.addRole(
+			Role accountRole = _roleLocalService.addRole(
 				defaultUser.getUserId(), null, 0,
 				AccountRoleConstants.REQUIRED_ROLE_NAME_ACCOUNT_MANAGER, null,
 				_roleDescriptionsMaps.get(
 					AccountRoleConstants.REQUIRED_ROLE_NAME_ACCOUNT_MANAGER),
 				RoleConstants.TYPE_ORGANIZATION, null, null);
+
+			_addResourcePermissions(
+				accountRole.getRoleId(), _accountMemberResourceActionsMap);
+			_addResourcePermissions(
+				accountRole.getRoleId(),
+				_accountAdministratorResourceActionsMap);
+			_addResourcePermissions(
+				accountRole.getRoleId(), _accountManagerResourceActionsMap);
 		}
 	}
 
@@ -156,6 +166,19 @@ public class AddDefaultAccountRolesPortalInstanceLifecycleListener
 		).put(
 			AccountEntry.class.getName(),
 			new String[] {ActionKeys.UPDATE, ActionKeys.MANAGE_USERS}
+		).build();
+	private static final Map<String, String[]>
+		_accountManagerResourceActionsMap = HashMapBuilder.put(
+			AccountEntry.class.getName(), new String[] {ActionKeys.DELETE}
+		).put(
+			AccountPortletKeys.ACCOUNT_ENTRIES_ADMIN,
+			new String[] {ActionKeys.ACCESS_IN_CONTROL_PANEL}
+		).put(
+			AccountPortletKeys.ACCOUNT_USERS_ADMIN,
+			new String[] {ActionKeys.ACCESS_IN_CONTROL_PANEL}
+		).put(
+			Organization.class.getName(),
+			new String[] {ActionKeys.MANAGE_ACCOUNTS}
 		).build();
 	private static final Map<String, String[]>
 		_accountMemberResourceActionsMap = HashMapBuilder.put(
