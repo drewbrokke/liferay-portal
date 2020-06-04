30d73a178ea0388d1e2198649686677607884d9c
Joshua Cords <joshua.cords@liferay.com>
2020-06-04 16:14:16 -0700

    LPS-114717 account-service: fix ambiguous call
    https://issues.liferay.com/browse/LPS-114717

--- modules/apps/account/account-service/src/main/java/com/liferay/account/service/impl/AccountEntryServiceImpl.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/service/impl/AccountEntryServiceImpl.java
@@ -28,6 +28,7 @@ import com.liferay.portal.kernel.model.OrganizationConstants;
 import com.liferay.portal.kernel.model.OrganizationModel;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.BaseModelSearchResult;
+import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
@@ -135,12 +136,14 @@ public class AccountEntryServiceImpl extends AccountEntryServiceBaseImpl {
 								permissionChecker, organization))
 					).build();
 
+				Sort[] sorts = null;
+
 				BaseModelSearchResult<Organization> baseModelSearchResult =
 					_organizationLocalService.searchOrganizations(
 						user.getCompanyId(),
 						OrganizationConstants.ANY_PARENT_ORGANIZATION_ID, null,
 						organizationParams, QueryUtil.ALL_POS,
-						QueryUtil.ALL_POS, null);
+						QueryUtil.ALL_POS, sorts);
 
 				if (baseModelSearchResult.getLength() == 0) {
 					return new BaseModelSearchResult<>(
