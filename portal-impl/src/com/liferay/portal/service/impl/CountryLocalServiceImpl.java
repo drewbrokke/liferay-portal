d37bd37a4a6d70c7b894906bb0d0ee4ef4a748bc
alee8888 <albert.lee@liferay.com>
2020-11-05 08:42:32 -0800

    LPS-121998 Add base methods to support Commerce

--- portal-impl/src/com/liferay/portal/service/impl/CountryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/CountryLocalServiceImpl.java
@@ -14,11 +14,117 @@
 
 package com.liferay.portal.service.impl;
 
+import com.liferay.portal.kernel.model.Country;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.service.base.CountryLocalServiceBaseImpl;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Brian Wing Shun Chan
  * @see CountryLocalServiceBaseImpl
  */
 public class CountryLocalServiceImpl extends CountryLocalServiceBaseImpl {
+
+	@Override
+	public Country addCountry(
+		String a2, String a3, boolean active, boolean billingAllowed,
+		String idd, String name, String number, double position,
+		boolean shippingAllowed, boolean subjectToVAT, boolean zipRequired,
+		Map<String, String> titleMap, ServiceContext serviceContext) {
+
+		return null;
+	}
+
+	@Override
+	public void deleteCountries(long companyId) {
+	}
+
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public Country deleteCountry(Country country) {
+		return null;
+	}
+
+	@Override
+	public Country deleteCountry(long countryId) {
+		return null;
+	}
+
+	@Override
+	public Country fetchCountryByCompanyIdAndA2(long companyId, String a2) {
+		return null;
+	}
+
+	@Override
+	public Country fetchCountryByCompanyIdAndNumber(
+		long companyId, String number) {
+
+		return null;
+	}
+
+	@Override
+	public List<Country> getCountriesByCompanyId(long companyId) {
+		return null;
+	}
+
+	@Override
+	public List<Country> getCountriesByCompanyId(
+		long companyId, int start, int end,
+		OrderByComparator<Country> orderByComparator) {
+
+		return null;
+	}
+
+	@Override
+	public List<Country> getCountriesByCompanyIdAndActive(
+		long companyId, boolean active, int start, int end,
+		OrderByComparator<Country> orderByComparator) {
+
+		return null;
+	}
+
+	@Override
+	public int getCountriesCountByCompanyId(long companyId) {
+		return null;
+	}
+
+	@Override
+	public int getCountriesCountByCompanyIdAndActive(
+		long companyId, boolean active) {
+
+		return null;
+	}
+
+	@Override
+	public Country getCountryByCompanyIdAndA2(long companyId, String a2) {
+		return null;
+	}
+
+	@Override
+	public Country setActive(long countryId, boolean active) {
+		return null;
+	}
+
+	@Override
+	public Country updateCountry(
+		long countryId, String a2, String a3, boolean active,
+		boolean billingAllowed, String idd, String name, String number,
+		double position, boolean shippingAllowed, boolean subjectToVAT,
+		Map<String, String> titleMap) {
+
+		return null;
+	}
+
+	@Override
+	public Country updateCountryGroupFilter(
+			long countryId, boolean groupFilterEnabled) {
+
+		return null;
+	}
+
 }
