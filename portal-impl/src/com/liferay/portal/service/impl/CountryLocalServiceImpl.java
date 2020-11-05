39523ab22167ecd8dcc7767d880a1683ec6cca36
alee8888 <albert.lee@liferay.com>
2020-11-05 08:42:32 -0800

    LPS-121998 Add base methods to support Commerce

--- portal-impl/src/com/liferay/portal/service/impl/CountryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/CountryLocalServiceImpl.java
@@ -25,6 +25,7 @@ import com.liferay.portal.kernel.model.Country;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.service.base.CountryLocalServiceBaseImpl;
 
@@ -87,13 +88,85 @@ public class CountryLocalServiceImpl extends CountryLocalServiceBaseImpl {
 	}
 
 	@Override
+	public Country deleteCountry(Country country) {
+		return null;
+	}
+
+	@Override
+	public Country deleteCountry(long countryId) {
+		return null;
+	}
+
+	@Override
+	public Country fetchCompanyCountry(long companyId, String a2) {
+		return null;
+	}
+
+	@Override
+	public Country fetchCompanyCountry(
+		long companyId, String number) {
+
+		return null;
+	}
+
+	@Override
 	public List<Country> getCompanyCountries(long companyId) {
 		return countryPersistence.findByCompanyId(companyId);
 	}
 
 	@Override
+	public List<Country> getCompanyCountries(
+		long companyId, int start, int end,
+		OrderByComparator<Country> orderByComparator) {
+
+		return null;
+	}
+
+	@Override
+	public List<Country> getCompanyCountries(
+		long companyId, boolean active, int start, int end,
+		OrderByComparator<Country> orderByComparator) {
+
+		return null;
+	}
+
+	@Override
 	public int getCompanyCountriesCount(long companyId) {
 		return countryPersistence.countByCompanyId(companyId);
+	}
+
+	@Override
+	public int getCompanyCountriesCount(
+		long companyId, boolean active) {
+
+		return null;
+	}
+
+	@Override
+	public Country getCompanyCountry(long companyId, String a2) {
+		return null;
+	}
+
+	@Override
+	public Country setActive(long countryId, boolean active) {
+		return null;
+	}
+
+	@Override
+	public Country updateCountry(
+		long countryId, String a2, String a3, boolean active,
+		boolean billingAllowed, String idd, String name, String number,
+		double position, boolean shippingAllowed, boolean subjectToVAT,
+		Map<String, String> titleMap) {
+
+		return null;
+	}
+
+	@Override
+	public Country updateCountryGroupFilter(
+			long countryId, boolean groupFilterEnabled) {
+
+		return null;
 	}
 
 	protected void validate(
