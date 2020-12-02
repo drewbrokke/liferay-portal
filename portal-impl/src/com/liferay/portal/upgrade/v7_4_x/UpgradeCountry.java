35e0777168ccb276e67daaa7e21e65a381732ef7
hai.yu <8219754@liferay.com>
2020-12-02 19:53:29 +0800

    LPS-124277 recorrect sql for oracle.

--- portal-impl/src/com/liferay/portal/upgrade/v7_4_x/UpgradeCountry.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/UpgradeCountry.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.upgrade.v7_4_x;
 
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.LoggingTimer;
@@ -41,10 +40,9 @@ public class UpgradeCountry extends UpgradeProcess {
 
 		String sql = StringBundler.concat(
 			"select User_.companyId, User_.userId from User_ join Company on ",
-			"User_.companyId = Company.companyId where User_.defaultUser = ",
-			"true and Company.webId = ",
-			StringUtil.quote(
-				PropsValues.COMPANY_DEFAULT_WEB_ID, StringPool.QUOTE));
+			"User_.companyId = Company.companyId where User_.defaultUser = 1 ",
+			"and Company.webId = ",
+			StringUtil.quote(PropsValues.COMPANY_DEFAULT_WEB_ID));
 
 		try (PreparedStatement ps = connection.prepareStatement(sql);
 			ResultSet rs = ps.executeQuery()) {
@@ -105,8 +103,8 @@ public class UpgradeCountry extends UpgradeProcess {
 		}
 
 		runSQL(
-			"update Country set billingAllowed = true where billingAllowed " +
-				"is null");
+			"update Country set billingAllowed = 1 where billingAllowed is " +
+				"null");
 
 		if (!hasColumn("Country", "groupFilterEnabled")) {
 			alter(
@@ -115,7 +113,7 @@ public class UpgradeCountry extends UpgradeProcess {
 		}
 
 		runSQL(
-			"update Country set groupFilterEnabled = false where " +
+			"update Country set groupFilterEnabled = 0 where " +
 				"groupFilterEnabled is null");
 
 		if (!hasColumn("Country", "position")) {
@@ -131,8 +129,8 @@ public class UpgradeCountry extends UpgradeProcess {
 		}
 
 		runSQL(
-			"update Country set shippingAllowed = true where shippingAllowed " +
-				"is null");
+			"update Country set shippingAllowed = 1 where shippingAllowed is " +
+				"null");
 
 		if (!hasColumn("Country", "subjectToVAT")) {
 			alter(
@@ -141,8 +139,7 @@ public class UpgradeCountry extends UpgradeProcess {
 		}
 
 		runSQL(
-			"update Country set subjectToVAT = false where subjectToVAT is " +
-				"null");
+			"update Country set subjectToVAT = 0 where subjectToVAT is null");
 
 		if (!hasColumn("Country", "lastPublishDate")) {
 			alter(
