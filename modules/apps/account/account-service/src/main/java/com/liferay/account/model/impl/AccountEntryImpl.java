f5c7ca5047252bf12de550e6471d0ff808b31f27
drewbrokke <drew.brokke@liferay.com>
2020-12-18 11:47:15 -0600

    LPS-124982 account-service: adds AccountEntryImpl convenience methods

--- modules/apps/account/account-service/src/main/java/com/liferay/account/model/impl/AccountEntryImpl.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/model/impl/AccountEntryImpl.java
@@ -14,13 +14,21 @@
 
 package com.liferay.account.model.impl;
 
+import com.liferay.account.constants.AccountConstants;
 import com.liferay.account.model.AccountEntry;
+import com.liferay.account.model.AccountEntryOrganizationRel;
+import com.liferay.account.model.AccountEntryUserRel;
+import com.liferay.account.service.AccountEntryLocalServiceUtil;
+import com.liferay.account.service.AccountEntryOrganizationRelLocalServiceUtil;
+import com.liferay.account.service.AccountEntryUserRelLocalServiceUtil;
 import com.liferay.portal.kernel.model.Address;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.service.AddressLocalServiceUtil;
 import com.liferay.portal.kernel.service.ClassNameLocalServiceUtil;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+
+import java.util.List;
 
 /**
  * @author Brian Wing Shun Chan
@@ -47,6 +55,18 @@ public class AccountEntryImpl extends AccountEntryBaseImpl {
 	}
 
 	@Override
+	public List<AccountEntryOrganizationRel> getAccountEntryOrganizationRels() {
+		return AccountEntryOrganizationRelLocalServiceUtil.
+			getAccountEntryOrganizationRels(getAccountEntryId());
+	}
+
+	@Override
+	public List<AccountEntryUserRel> getAccountEntryUserRels() {
+		return AccountEntryUserRelLocalServiceUtil.
+			getAccountEntryUserRelsByAccountEntryId(getAccountEntryId());
+	}
+
+	@Override
 	public Address getDefaultBillingAddress() {
 		return AddressLocalServiceUtil.fetchAddress(
 			getDefaultBillingAddressId());
@@ -58,11 +78,55 @@ public class AccountEntryImpl extends AccountEntryBaseImpl {
 			getDefaultShippingAddressId());
 	}
 
+	@Override
+	public AccountEntry getParentAccountEntry() {
+		if (isRoot()) {
+			return null;
+		}
+
+		return AccountEntryLocalServiceUtil.fetchAccountEntry(
+			getParentAccountEntryId());
+	}
+
+	@Override
+	public boolean isBusinessAccount() {
+		return _isType(AccountConstants.ACCOUNT_ENTRY_TYPE_BUSINESS);
+	}
+
+	@Override
+	public boolean isGuestAccount() {
+		return _isType(AccountConstants.ACCOUNT_ENTRY_TYPE_GUEST);
+	}
+
+	@Override
+	public boolean isPersonAccount() {
+		return _isType(AccountConstants.ACCOUNT_ENTRY_TYPE_PERSON);
+	}
+
+	@Override
+	public boolean isRoot() {
+		if (getParentAccountEntryId() ==
+				AccountConstants.PARENT_ACCOUNT_ENTRY_ID_DEFAULT) {
+
+			return true;
+		}
+
+		return false;
+	}
+
 	private Group _getAccountEntryGroup() {
 		return GroupLocalServiceUtil.fetchGroup(
 			getCompanyId(),
 			ClassNameLocalServiceUtil.getClassNameId(AccountEntry.class),
 			getAccountEntryId());
+	}
+
+	private boolean _isType(String type) {
+		if (getType() == type) {
+			return true;
+		}
+
+		return false;
 	}
 
 }
