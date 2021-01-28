cbb479926670c5b16e7748f48893f25e6b4fa3f2
pei-jung <pei-jung.lan@liferay.com>
2021-01-28 06:10:13 -0800

    LPS-125991 (commerce-warehouse-web) Replace services

--- modules/apps/commerce/commerce-warehouse-web/src/main/java/com/liferay/commerce/warehouse/web/internal/portlet/CommerceInventoryWarehousePortlet.java
+++ modules/apps/commerce/commerce-warehouse-web/src/main/java/com/liferay/commerce/warehouse/web/internal/portlet/CommerceInventoryWarehousePortlet.java
@@ -18,9 +18,9 @@ import com.liferay.commerce.inventory.service.CommerceInventoryWarehouseService;
 import com.liferay.commerce.product.constants.CPPortletKeys;
 import com.liferay.commerce.product.service.CommerceChannelRelService;
 import com.liferay.commerce.product.service.CommerceChannelService;
-import com.liferay.commerce.service.CommerceCountryService;
 import com.liferay.commerce.warehouse.web.internal.display.context.CommerceInventoryWarehousesDisplayContext;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.service.CountryService;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
 
@@ -71,7 +71,7 @@ public class CommerceInventoryWarehousePortlet extends MVCPortlet {
 			commerceInventoryWarehousesDisplayContext =
 				new CommerceInventoryWarehousesDisplayContext(
 					_commerceChannelRelService, _commerceChannelService,
-					_commerceCountryService, _commerceInventoryWarehouseService,
+					_countryService, _commerceInventoryWarehouseService,
 					_portal.getHttpServletRequest(renderRequest));
 
 		renderRequest.setAttribute(
@@ -88,11 +88,11 @@ public class CommerceInventoryWarehousePortlet extends MVCPortlet {
 	private CommerceChannelService _commerceChannelService;
 
 	@Reference
-	private CommerceCountryService _commerceCountryService;
-
-	@Reference
 	private CommerceInventoryWarehouseService
 		_commerceInventoryWarehouseService;
+
+	@Reference
+	private CountryService _countryService;
 
 	@Reference
 	private Portal _portal;
