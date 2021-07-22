e1b2bd11e000e84d581a5404f8d00a713427481e
drewbrokke <drew.brokke@liferay.com>
2021-07-22 14:08:52 -0500

    LPS-136087 headless-admin-user-test: overrides test cases to account for shared account roles being included in result

--- modules/apps/headless/headless-admin-user/headless-admin-user-test/src/testIntegration/java/com/liferay/headless/admin/user/resource/v1_0/test/AccountRoleResourceTest.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-test/src/testIntegration/java/com/liferay/headless/admin/user/resource/v1_0/test/AccountRoleResourceTest.java
@@ -14,6 +14,7 @@
 
 package com.liferay.headless.admin.user.resource.v1_0.test;
 
+import com.liferay.account.constants.AccountConstants;
 import com.liferay.account.model.AccountEntry;
 import com.liferay.account.service.AccountEntryLocalService;
 import com.liferay.account.service.AccountRoleLocalService;
@@ -26,18 +27,30 @@ import com.liferay.headless.admin.user.client.dto.v1_0.PostalAddress;
 import com.liferay.headless.admin.user.client.dto.v1_0.UserAccount;
 import com.liferay.headless.admin.user.client.dto.v1_0.UserAccountContactInformation;
 import com.liferay.headless.admin.user.client.dto.v1_0.WebUrl;
+import com.liferay.headless.admin.user.client.pagination.Page;
+import com.liferay.headless.admin.user.client.pagination.Pagination;
 import com.liferay.headless.admin.user.client.resource.v1_0.AccountResource;
 import com.liferay.headless.admin.user.client.resource.v1_0.UserAccountResource;
+import com.liferay.headless.admin.user.client.serdes.v1_0.AccountRoleSerDes;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.UserGroupRole;
 import com.liferay.portal.kernel.service.UserGroupRoleLocalService;
 import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.CalendarFactoryUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.vulcan.util.TransformUtil;
 
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -72,6 +85,25 @@ public class AccountRoleResourceTest extends BaseAccountRoleResourceTestCase {
 		).build();
 
 		_account = _accountResource.postAccount(_randomAccount());
+
+		_sharedAccountRoles = TransformUtil.transform(
+			_accountRoleLocalService.getAccountRolesByAccountEntryIds(
+				new long[] {AccountConstants.ACCOUNT_ENTRY_ID_DEFAULT}),
+			serviceBuilderAccountRole -> {
+				Role role = serviceBuilderAccountRole.getRole();
+
+				return new AccountRole() {
+					{
+						accountId =
+							serviceBuilderAccountRole.getAccountEntryId();
+						description = role.getDescription();
+						displayName = role.getTitle();
+						id = serviceBuilderAccountRole.getAccountRoleId();
+						name = serviceBuilderAccountRole.getRoleName();
+						roleId = serviceBuilderAccountRole.getRoleId();
+					}
+				};
+			});
 	}
 
 	@After
@@ -127,6 +159,245 @@ public class AccountRoleResourceTest extends BaseAccountRoleResourceTestCase {
 
 		_assertAccountRoleUserAssociation(
 			_account, accountRole, accountUser, false);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testGetAccountRolesByExternalReferenceCodePage()
+		throws Exception {
+
+		Page<AccountRole> page =
+			accountRoleResource.getAccountRolesByExternalReferenceCodePage(
+				testGetAccountRolesByExternalReferenceCodePage_getExternalReferenceCode(),
+				RandomTestUtil.randomString(), Pagination.of(1, 2), null);
+
+		Assert.assertEquals(0, page.getTotalCount());
+
+		String externalReferenceCode =
+			testGetAccountRolesByExternalReferenceCodePage_getExternalReferenceCode();
+
+		String irrelevantExternalReferenceCode =
+			testGetAccountRolesByExternalReferenceCodePage_getIrrelevantExternalReferenceCode();
+
+		if (irrelevantExternalReferenceCode != null) {
+			AccountRole irrelevantAccountRole =
+				testGetAccountRolesByExternalReferenceCodePage_addAccountRole(
+					irrelevantExternalReferenceCode,
+					randomIrrelevantAccountRole());
+
+			page =
+				accountRoleResource.getAccountRolesByExternalReferenceCodePage(
+					irrelevantExternalReferenceCode, null, Pagination.of(1, 2),
+					null);
+
+			Assert.assertEquals(1, page.getTotalCount());
+
+			assertEquals(
+				Arrays.asList(irrelevantAccountRole),
+				(List<AccountRole>)page.getItems());
+			assertValid(page);
+		}
+
+		List<AccountRole> expectedAccountRoles = ListUtil.concat(
+			Arrays.asList(
+				testGetAccountRolesByExternalReferenceCodePage_addAccountRole(
+					externalReferenceCode, randomAccountRole()),
+				testGetAccountRolesByExternalReferenceCodePage_addAccountRole(
+					externalReferenceCode, randomAccountRole())),
+			_sharedAccountRoles);
+
+		page = accountRoleResource.getAccountRolesByExternalReferenceCodePage(
+			externalReferenceCode, null,
+			Pagination.of(1, expectedAccountRoles.size()), null);
+
+		Assert.assertEquals(expectedAccountRoles.size(), page.getTotalCount());
+
+		assertEqualsIgnoringOrder(
+			expectedAccountRoles, (List<AccountRole>)page.getItems());
+		assertValid(page);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testGetAccountRolesByExternalReferenceCodePageWithPagination()
+		throws Exception {
+
+		String externalReferenceCode =
+			testGetAccountRolesByExternalReferenceCodePage_getExternalReferenceCode();
+
+		List<AccountRole> expectedAccountRoles = ListUtil.concat(
+			Arrays.asList(
+				testGetAccountRolesByExternalReferenceCodePage_addAccountRole(
+					externalReferenceCode, randomAccountRole()),
+				testGetAccountRolesByExternalReferenceCodePage_addAccountRole(
+					externalReferenceCode, randomAccountRole()),
+				testGetAccountRolesByExternalReferenceCodePage_addAccountRole(
+					externalReferenceCode, randomAccountRole())),
+			_sharedAccountRoles);
+
+		Page<AccountRole> page1 =
+			accountRoleResource.getAccountRolesByExternalReferenceCodePage(
+				externalReferenceCode, null, Pagination.of(1, 2), null);
+
+		List<AccountRole> accountRoles1 = (List<AccountRole>)page1.getItems();
+
+		Assert.assertEquals(accountRoles1.toString(), 2, accountRoles1.size());
+
+		Page<AccountRole> page2 =
+			accountRoleResource.getAccountRolesByExternalReferenceCodePage(
+				externalReferenceCode, null,
+				Pagination.of(2, expectedAccountRoles.size() - 1), null);
+
+		Assert.assertEquals(expectedAccountRoles.size(), page2.getTotalCount());
+
+		List<AccountRole> accountRoles2 = (List<AccountRole>)page2.getItems();
+
+		Assert.assertEquals(accountRoles2.toString(), 1, accountRoles2.size());
+
+		Page<AccountRole> page3 =
+			accountRoleResource.getAccountRolesByExternalReferenceCodePage(
+				externalReferenceCode, null,
+				Pagination.of(1, expectedAccountRoles.size()), null);
+
+		assertEqualsIgnoringOrder(
+			expectedAccountRoles, (List<AccountRole>)page3.getItems());
+	}
+
+	@Override
+	@Test
+	public void testGetAccountRolesPage() throws Exception {
+		Page<AccountRole> page = accountRoleResource.getAccountRolesPage(
+			testGetAccountRolesPage_getAccountId(),
+			RandomTestUtil.randomString(), Pagination.of(1, 2), null);
+
+		Assert.assertEquals(0, page.getTotalCount());
+
+		Long accountId = testGetAccountRolesPage_getAccountId();
+
+		Long irrelevantAccountId =
+			testGetAccountRolesPage_getIrrelevantAccountId();
+
+		if (irrelevantAccountId != null) {
+			AccountRole irrelevantAccountRole =
+				testGetAccountRolesPage_addAccountRole(
+					irrelevantAccountId, randomIrrelevantAccountRole());
+
+			page = accountRoleResource.getAccountRolesPage(
+				irrelevantAccountId, null, Pagination.of(1, 2), null);
+
+			Assert.assertEquals(1, page.getTotalCount());
+
+			assertEquals(
+				Collections.singletonList(irrelevantAccountRole),
+				(List<AccountRole>)page.getItems());
+			assertValid(page);
+		}
+
+		AccountRole accountRole1 = testGetAccountRolesPage_addAccountRole(
+			accountId, randomAccountRole());
+
+		AccountRole accountRole2 = testGetAccountRolesPage_addAccountRole(
+			accountId, randomAccountRole());
+
+		page = accountRoleResource.getAccountRolesPage(
+			accountId, null, Pagination.of(1, 2), null);
+
+		Assert.assertEquals(_addSharedAccountRoles(2), page.getTotalCount());
+
+		assertEqualsIgnoringOrder(
+			Arrays.asList(accountRole1, accountRole2),
+			(List<AccountRole>)page.getItems());
+		assertValid(page);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testGetAccountRolesPageWithPagination() throws Exception {
+		Long accountId = testGetAccountRolesPage_getAccountId();
+
+		List<AccountRole> expectedAccountRoles = ListUtil.concat(
+			Arrays.asList(
+				testGetAccountRolesPage_addAccountRole(
+					accountId, randomAccountRole()),
+				testGetAccountRolesPage_addAccountRole(
+					accountId, randomAccountRole()),
+				testGetAccountRolesPage_addAccountRole(
+					accountId, randomAccountRole())),
+			_sharedAccountRoles);
+
+		Page<AccountRole> page1 = accountRoleResource.getAccountRolesPage(
+			accountId, null, Pagination.of(1, 2), null);
+
+		List<AccountRole> accountRoles1 = (List<AccountRole>)page1.getItems();
+
+		Assert.assertEquals(accountRoles1.toString(), 2, accountRoles1.size());
+
+		Page<AccountRole> page2 = accountRoleResource.getAccountRolesPage(
+			accountId, null, Pagination.of(2, expectedAccountRoles.size() - 1),
+			null);
+
+		Assert.assertEquals(expectedAccountRoles.size(), page2.getTotalCount());
+
+		List<AccountRole> accountRoles2 = (List<AccountRole>)page2.getItems();
+
+		Assert.assertEquals(accountRoles2.toString(), 1, accountRoles2.size());
+
+		Page<AccountRole> page3 = accountRoleResource.getAccountRolesPage(
+			accountId, null, Pagination.of(1, expectedAccountRoles.size()),
+			null);
+
+		assertEqualsIgnoringOrder(
+			expectedAccountRoles, (List<AccountRole>)page3.getItems());
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testGraphQLGetAccountRolesPage() throws Exception {
+		Long accountId = testGetAccountRolesPage_getAccountId();
+
+		GraphQLField graphQLField = new GraphQLField(
+			"accountRoles",
+			HashMapBuilder.<String, Object>put(
+				"accountId", accountId
+			).put(
+				"page", 1
+			).put(
+				"pageSize", _sharedAccountRoles.size() + 2
+			).build(),
+			new GraphQLField("items", getGraphQLFields()),
+			new GraphQLField("page"), new GraphQLField("totalCount"));
+
+		JSONObject accountRolesJSONObject = JSONUtil.getValueAsJSONObject(
+			invokeGraphQLQuery(graphQLField), "JSONObject/data",
+			"JSONObject/accountRoles");
+
+		Assert.assertEquals(
+			_sharedAccountRoles.size(),
+			accountRolesJSONObject.get("totalCount"));
+
+		List<AccountRole> expectedAccountRoles = ListUtil.concat(
+			Arrays.asList(
+				testGraphQLAccountRole_addAccountRole(),
+				testGraphQLAccountRole_addAccountRole()),
+			_sharedAccountRoles);
+
+		accountRolesJSONObject = JSONUtil.getValueAsJSONObject(
+			invokeGraphQLQuery(graphQLField), "JSONObject/data",
+			"JSONObject/accountRoles");
+
+		Assert.assertEquals(
+			expectedAccountRoles.size(),
+			accountRolesJSONObject.get("totalCount"));
+
+		assertEqualsIgnoringOrder(
+			expectedAccountRoles,
+			Arrays.asList(
+				AccountRoleSerDes.toDTOs(
+					accountRolesJSONObject.getString("items"))));
 	}
 
 	@Override
@@ -282,6 +553,10 @@ public class AccountRoleResourceTest extends BaseAccountRoleResourceTestCase {
 	private UserAccount _addAccountUser(Account account) throws Exception {
 		return _userAccountResource.postAccountUser(
 			account.getId(), _randomAccountUser());
+	}
+
+	private int _addSharedAccountRoles(int count) {
+		return count + _sharedAccountRoles.size();
 	}
 
 	private void _assertAccountRoleUserAssociation(
@@ -449,6 +724,7 @@ public class AccountRoleResourceTest extends BaseAccountRoleResourceTestCase {
 	@Inject
 	private AccountRoleLocalService _accountRoleLocalService;
 
+	private List<AccountRole> _sharedAccountRoles;
 	private UserAccountResource _userAccountResource;
 
 	@Inject
