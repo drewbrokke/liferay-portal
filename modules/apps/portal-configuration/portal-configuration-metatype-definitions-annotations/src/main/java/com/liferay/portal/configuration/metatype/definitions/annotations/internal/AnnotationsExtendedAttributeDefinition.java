d9d31a41026fdd921ee8be04fabbde0b938b9e9a
drewbrokke <drew.brokke@liferay.com>
2021-11-11 17:49:51 -0600

    --wip--

--- modules/apps/portal-configuration/portal-configuration-metatype-definitions-annotations/src/main/java/com/liferay/portal/configuration/metatype/definitions/annotations/internal/AnnotationsExtendedAttributeDefinition.java
+++ modules/apps/portal-configuration/portal-configuration-metatype-definitions-annotations/src/main/java/com/liferay/portal/configuration/metatype/definitions/annotations/internal/AnnotationsExtendedAttributeDefinition.java
@@ -30,6 +30,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.osgi.service.metatype.AttributeDefinition;
+import org.osgi.service.metatype.MetaTypeInformation;
 
 /**
  * @author Iván Zaera
