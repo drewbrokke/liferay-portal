0022b6260b0b75b667f08b57f443bfe825928ba3
drewbrokke <drew.brokke@liferay.com>
2022-03-11 17:19:27 -0600

    copies code into the 'poshi-vscode' module

---
+++ modules/test/poshi/poshi-vscode/src/extension.ts
@@ -0,0 +1,42 @@
+import * as vscode from 'vscode';
+
+import { CompletionItemProviderImpl } from './lib/languageFeatureProviders/CompletionItemProviderImpl';
+import { DefinitionProviderImpl } from './lib/languageFeatureProviders/DefinitionProviderImpl';
+import { DocumentFormattingEditProviderImpl } from './lib/languageFeatureProviders/DocumentFormattingEditProviderImpl';
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log(
+		'Registering language feature providers...',
+	);
+
+	context.subscriptions.push(
+		vscode.languages.registerCompletionItemProvider(
+			{ pattern: '**/*.{function,macro,testcase}' },
+			new CompletionItemProviderImpl(context),
+			'.',
+			' ',
+		),
+	);
+	context.subscriptions.push(
+		vscode.languages.registerDefinitionProvider(
+			{
+				pattern: '**/*.{function,macro,testcase}',
+			},
+			new DefinitionProviderImpl(),
+		),
+	);
+	context.subscriptions.push(
+		vscode.languages.registerDocumentFormattingEditProvider(
+			{
+				pattern: '**/*.{function,macro,testcase}',
+			},
+			new DocumentFormattingEditProviderImpl(),
+		),
+	);
+
+	console.log(
+		'Congratulations, your extension "poshi-language-support" is now active!',
+	);
+}
+
+export function deactivate() {}
