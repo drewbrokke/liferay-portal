87868811599d090f324d4e63c6b97c2c18d4d1c7
drewbrokke <drew.brokke@liferay.com>
2022-04-27 11:47:53 -0500

    LPS-151908 headless-admin-address: implements post method

--- modules/apps/headless/headless-admin-address/headless-admin-address-impl/src/main/java/com/liferay/headless/admin/address/internal/resource/v1_0/CountryResourceImpl.java
+++ modules/apps/headless/headless-admin-address/headless-admin-address-impl/src/main/java/com/liferay/headless/admin/address/internal/resource/v1_0/CountryResourceImpl.java
@@ -20,8 +20,11 @@ import com.liferay.headless.admin.address.resource.v1_0.CountryResource;
 import com.liferay.portal.kernel.model.CountryTable;
 import com.liferay.portal.kernel.search.BaseModelSearchResult;
 import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.service.CountryLocalService;
 import com.liferay.portal.kernel.service.CountryService;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.OrderByComparatorFactoryUtil;
 import com.liferay.portal.odata.entity.DoubleEntityField;
@@ -105,6 +108,28 @@ public class CountryResourceImpl extends BaseCountryResourceImpl {
 			new DoubleEntityField("position", locale -> "position"));
 	}
 
+	@Override
+	public Country postCountry(Country country) throws Exception {
+		com.liferay.portal.kernel.model.Country serviceBuilderCountry =
+			_countryService.addCountry(
+				country.getA2(), country.getA3(),
+				GetterUtil.getBoolean(country.getActive(), true),
+				GetterUtil.getBoolean(country.getBillingAllowed(), true),
+				String.valueOf(country.getIdd()), country.getName(),
+				String.valueOf(country.getNumber()),
+				GetterUtil.getDouble(country.getPosition()),
+				GetterUtil.getBoolean(country.getShippingAllowed(), true),
+				GetterUtil.getBoolean(country.getSubjectToVAT()),
+				GetterUtil.getBoolean(country.getZipRequired(), true),
+				ServiceContextFactory.getInstance(
+					Country.class.getName(), contextHttpServletRequest));
+
+		return _toCountry(
+			_countryLocalService.updateGroupFilterEnabled(
+				serviceBuilderCountry.getCountryId(),
+				GetterUtil.getBoolean(country.getGroupFilterEnabled())));
+	}
+
 	private Country _toCountry(
 			com.liferay.portal.kernel.model.Country serviceBuilderCountry)
 		throws Exception {
@@ -130,6 +155,9 @@ public class CountryResourceImpl extends BaseCountryResourceImpl {
 			CountryTable.INSTANCE.getTableName(),
 			objects.toArray(new Object[0]));
 	}
+
+	@Reference
+	private CountryLocalService _countryLocalService;
 
 	@Reference
 	private CountryResourceDTOConverter _countryResourceDTOConverter;
