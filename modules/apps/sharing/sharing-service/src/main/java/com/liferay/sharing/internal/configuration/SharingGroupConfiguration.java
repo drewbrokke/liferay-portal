1eaec3da79c122279279f7839de9247261e23583
drewbrokke <drew.brokke@liferay.com>
2022-12-08 15:41:27 -0600

    LPS-170759 sharing-service: apply strictScope attribute

--- modules/apps/sharing/sharing-service/src/main/java/com/liferay/sharing/internal/configuration/SharingGroupConfiguration.java
+++ modules/apps/sharing/sharing-service/src/main/java/com/liferay/sharing/internal/configuration/SharingGroupConfiguration.java
@@ -23,7 +23,7 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
  */
 @ExtendedObjectClassDefinition(
 	category = "sharing", generateUI = false,
-	scope = ExtendedObjectClassDefinition.Scope.GROUP
+	scope = ExtendedObjectClassDefinition.Scope.GROUP, strictScope = true
 )
 @Meta.OCD(
 	id = "com.liferay.sharing.internal.configuration.SharingGroupConfiguration",
