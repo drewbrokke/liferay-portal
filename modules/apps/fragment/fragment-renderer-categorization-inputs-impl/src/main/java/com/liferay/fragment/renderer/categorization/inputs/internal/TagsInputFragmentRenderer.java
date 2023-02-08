0640124df2949f84c2e4b52bf64866d160b15fc2
drewbrokke <drew.brokke@liferay.com>
2023-02-08 14:08:59 -0600

    LPS-175159 fragment-renderer-categorization-inputs-impl: replaces feature flag reads with FeatureFlagManagerUtil

--- modules/apps/fragment/fragment-renderer-categorization-inputs-impl/src/main/java/com/liferay/fragment/renderer/categorization/inputs/internal/TagsInputFragmentRenderer.java
+++ modules/apps/fragment/fragment-renderer-categorization-inputs-impl/src/main/java/com/liferay/fragment/renderer/categorization/inputs/internal/TagsInputFragmentRenderer.java
@@ -36,15 +36,14 @@ import com.liferay.layout.util.structure.LayoutStructureItem;
 import com.liferay.layout.util.structure.LayoutStructureItemUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
@@ -98,7 +97,7 @@ public class TagsInputFragmentRenderer implements FragmentRenderer {
 
 	@Override
 	public boolean isSelectable(HttpServletRequest httpServletRequest) {
-		if (GetterUtil.getBoolean(PropsUtil.get("feature.flag.LPS-161631"))) {
+		if (FeatureFlagManagerUtil.isEnabled("LPS-161631")) {
 			return true;
 		}
 
