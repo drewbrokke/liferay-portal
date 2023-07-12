674e8e5667ff31d015dfca4b43aead1ac1c72737
Gregory Amerson <gregory.amerson@liferay.com>
2023-07-12 18:24:14 -0500

    LPS-187069 use projectId and add projectName

--- modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/internal/configuration/BaseConfigurationFactory.java
+++ modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/internal/configuration/BaseConfigurationFactory.java
@@ -92,11 +92,12 @@ public abstract class BaseConfigurationFactory {
 
 		_extensionProperties = extensionProperties;
 
-		String projectName = GetterUtil.getString(
-			properties.get("projectName"));
+		String projectId = GetterUtil.getString(properties.get("projectId"));
 
 		String serviceOrProjectId = GetterUtil.getString(
-			properties.get("ext.lxc.liferay.com.serviceId"), projectName);
+			properties.get("ext.lxc.liferay.com.serviceId"),
+			GetterUtil.getString(
+				properties.get("ext.lxc.liferay.com.projectId"), projectId));
 
 		PortalK8sConfigMapModifier portalK8sConfigMapModifier =
 			_portalK8sConfigMapModifierSnapshot.get();
@@ -126,7 +127,12 @@ public abstract class BaseConfigurationFactory {
 					"ext.lxc.liferay.com/projectId",
 					GetterUtil.getString(
 						properties.get("ext.lxc.liferay.com.projectId"),
-						projectName));
+						projectId));
+				labels.put(
+					"ext.lxc.liferay.com/projectName",
+					GetterUtil.getString(
+						properties.get("ext.lxc.liferay.com.projectName"),
+						GetterUtil.getString(properties.get("projectName"))));
 				labels.put(
 					"ext.lxc.liferay.com/projectUid",
 					GetterUtil.getString(
