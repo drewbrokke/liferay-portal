79d5fd0b97fd78320e2fad94fb1b6eabf90b71ed
Gregory Amerson <gregory.amerson@liferay.com>
2023-07-21 16:24:22 -0500

    LPS-187069 change path

--- workspaces/liferay-sample-workspace/client-extensions/liferay-sample-etc-cron/src/main/java/com/liferay/sample/SampleCommandLineRunner.java
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-etc-cron/src/main/java/com/liferay/sample/SampleCommandLineRunner.java
@@ -41,7 +41,7 @@ public class SampleCommandLineRunner implements CommandLineRunner {
 
 		java.util.logging.LogManager.getLogManager().getLogger("sun.net.www.protocol.http.HttpURLConnection").setLevel(java.util.logging.Level.ALL);
 
-		Site site = siteResource.getSiteByFriendlyUrlPath("guest");
+		Site site = siteResource.getSiteByFriendlyUrlPath("/guest");
 
 		MessageBoardThreadResource messageBoardThreadResource =
 			MessageBoardThreadResource.builder(
