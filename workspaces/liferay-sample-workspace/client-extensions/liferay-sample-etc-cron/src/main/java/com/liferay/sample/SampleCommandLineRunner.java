ddd7d0987c5a0f97563b51d6119c4299cd1ad454
Gregory Amerson <gregory.amerson@liferay.com>
2023-07-24 12:31:47 -0500

    LPS-187069 more debug

--- workspaces/liferay-sample-workspace/client-extensions/liferay-sample-etc-cron/src/main/java/com/liferay/sample/SampleCommandLineRunner.java
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-etc-cron/src/main/java/com/liferay/sample/SampleCommandLineRunner.java
@@ -12,6 +12,11 @@ import com.liferay.headless.delivery.client.pagination.Page;
 import com.liferay.headless.delivery.client.pagination.Pagination;
 import com.liferay.headless.delivery.client.resource.v1_0.MessageBoardThreadResource;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Collection;
 
 import org.apache.commons.logging.Log;
@@ -38,6 +43,38 @@ public class SampleCommandLineRunner implements CommandLineRunner {
 		).endpoint(
 			_lxcDXPMainDomain, _lxcDXPServerProtocol
 		).build();
+
+		String urlString = _lxcDXPServerProtocol + "://" + _lxcDXPMainDomain + "/o/headless-admin-user/v1.0/sites/by-friendly-url-path/guest";
+
+    try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                System.out.println("Response Body:");
+                System.out.println(response.toString());
+            } else {
+                System.out.println("GET request failed.");
+                System.out.println("responseCode: " + responseCode);
+            }
+
+            conn.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 		java.util.logging.LogManager.getLogManager().getLogger("sun.net.www.protocol.http.HttpURLConnection").setLevel(java.util.logging.Level.ALL);
 
