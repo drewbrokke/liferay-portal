f5173288e976b24fc7356db7b387ccbf2f095b03
Seiphon Wang <seiphon.wang@liferay.com>
2023-08-03 15:20:12 +0800

    LPS-187805 Add pnpm executor

---
+++ modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/internal/PnpmExecutor.java
@@ -0,0 +1,293 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.gradle.plugins.node.internal;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.gradle.api.Action;
+import org.gradle.api.Project;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
+import org.gradle.process.ExecSpec;
+import org.gradle.util.GUtil;
+
+import com.liferay.gradle.plugins.node.internal.util.NodePluginUtil;
+import com.liferay.gradle.util.GradleUtil;
+import com.liferay.gradle.util.OSDetector;
+import com.liferay.gradle.util.Validator;
+
+/**
+ * @author Seiphon Wang
+ */
+public class PnpmExecutor {
+
+	public PnpmExecutor(Project project) {
+		_project = project;
+
+		_workingDir = project.getProjectDir();
+	}
+
+	public PnpmExecutor args(Iterable<?> args) {
+		GUtil.addToCollection(_args, args);
+
+		return this;
+	}
+
+	public String execute() throws Exception {
+		File workingDir = getWorkingDir();
+
+		workingDir.mkdir();
+
+		if (isUseGradleExec()) {
+			return _executeGradleExec();
+		}
+
+		return _executeProcessBuilder();
+	}
+
+	private String _executeGradleExec() {
+		final ByteArrayOutputStream byteArrayOutputStream =
+			new ByteArrayOutputStream();
+
+		_project.exec(
+			new Action<ExecSpec>() {
+
+				@Override
+				public void execute(ExecSpec execSpec) {
+					execSpec.setCommandLine(_getCommandLine());
+					execSpec.setEnvironment(
+						_getEnvironment(execSpec.getEnvironment()));
+					execSpec.setErrorOutput(
+						new TeeOutputStream(byteArrayOutputStream, System.out));
+					execSpec.setStandardOutput(
+						new TeeOutputStream(byteArrayOutputStream, System.out));
+					execSpec.setWorkingDir(getWorkingDir());
+				}
+			});
+
+		String result = byteArrayOutputStream.toString();
+
+		return result.trim();
+	}
+
+	private String _executeProcessBuilder() throws Exception {
+		ProcessBuilder processBuilder = new ProcessBuilder(_getCommandLine());
+
+		processBuilder.directory(getWorkingDir());
+		processBuilder.redirectErrorStream(true);
+
+//		_updateEnvironment(processBuilder.environment());
+
+		if (_logger.isInfoEnabled()) {
+			_logger.info(
+				"Running {} from {} with environment variables {}",
+				processBuilder.command(), processBuilder.directory(),
+				processBuilder.environment());
+		}
+
+		Process process = processBuilder.start();
+
+		BufferedReader bufferedReader = new BufferedReader(
+			new InputStreamReader(process.getInputStream()));
+
+		StringBuilder sb = new StringBuilder();
+
+		String line = null;
+
+		while ((line = bufferedReader.readLine()) != null) {
+			System.out.println(line);
+
+			sb.append(line + System.lineSeparator());
+		}
+
+		int exitValue = process.waitFor();
+
+		if (exitValue != 0) {
+			throw new IOException(
+				"Process '" + processBuilder.command() +
+					"' finished with non-zero exit value " + exitValue);
+		}
+
+		String result = sb.toString();
+
+		return result.trim();
+	}
+
+	private Map<String, String> _getEnvironment(Map<?, ?> environment) {
+		Map<String, String> newEnvironment = new HashMap<>();
+
+		GUtil.addToMap(newEnvironment, environment);
+
+//		_updateEnvironment(newEnvironment);
+
+		return newEnvironment;
+	}
+
+	private List<String> _getCommandLine() {
+		List<String> commandLine = new ArrayList<>();
+
+		if (OSDetector.isWindows()) {
+			commandLine.add("cmd");
+			commandLine.addAll(_getWindowsArgs());
+		}
+		else {
+			commandLine.add(_getExecutable());
+			commandLine.addAll(GradleUtil.toStringList(getArgs()));
+		}
+
+		return commandLine;
+	}
+
+	private String _getExecutable() {
+		String executable = GradleUtil.toString(_command);
+
+		File executableDir = _getExecutableDir();
+
+		if (executableDir != null) {
+			File executableFile = new File(executableDir, executable);
+
+			executable = executableFile.getAbsolutePath();
+		}
+
+		return executable;
+	}
+
+	private File _getExecutableDir() {
+		File nodeDir = getNodeDir();
+
+		if (nodeDir == null) {
+			return null;
+		}
+
+		return NodePluginUtil.getBinDir(nodeDir);
+	}
+
+	public File getNodeDir() {
+		return GradleUtil.toFile(_project, _nodeDir);
+	}
+
+	public List<Object> getArgs() {
+		return _args;
+	}
+
+	private List<String> _getWindowsArgs() {
+		List<String> windowsArgs = new ArrayList<>(2);
+
+		windowsArgs.add("/c");
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append('"');
+
+		String executable = _getExecutable();
+
+		if (executable.indexOf(File.separatorChar) == -1) {
+			sb.append(executable);
+		}
+		else {
+			sb.append('"');
+			sb.append(executable);
+			sb.append('"');
+		}
+
+		List<String> args = GradleUtil.toStringList(getArgs());
+
+		for (String arg : args) {
+			sb.append(" \"");
+
+			if (Validator.isNotNull(arg)) {
+				sb.append(arg);
+			}
+
+			sb.append('"');
+		}
+
+		sb.append('"');
+
+		windowsArgs.add(sb.toString());
+
+		return windowsArgs;
+	}
+
+	public boolean isUseGradleExec() {
+		return _useGradleExec;
+	}
+
+	public File getWorkingDir() {
+		return GradleUtil.toFile(_project, _workingDir);
+	}
+
+	public String getCommand() {
+		return GradleUtil.toString(_command);
+	}
+
+	private static final Logger _logger = Logging.getLogger(PnpmExecutor.class);
+	private final List<Object> _args = new ArrayList<>();
+	private Object _command = "pnpm";
+	private Object _nodeDir;
+	private Project _project;
+	private boolean _useGradleExec;
+	private Object _workingDir;
+
+	private static class TeeOutputStream extends OutputStream {
+
+		public TeeOutputStream(
+			OutputStream outputStream1, OutputStream outputStream2) {
+
+			_outputStream1 = outputStream1;
+			_outputStream2 = outputStream2;
+		}
+
+		@Override
+		public void close() throws IOException {
+			try {
+				_outputStream1.close();
+			}
+			finally {
+				_outputStream2.close();
+			}
+		}
+
+		@Override
+		public void flush() throws IOException {
+			_outputStream1.flush();
+			_outputStream2.flush();
+		}
+
+		@Override
+		public synchronized void write(byte[] bytes) throws IOException {
+			_outputStream1.write(bytes);
+			_outputStream2.write(bytes);
+		}
+
+		@Override
+		public synchronized void write(byte[] bytes, int offset, int length)
+			throws IOException {
+
+			_outputStream1.write(bytes, offset, length);
+			_outputStream2.write(bytes, offset, length);
+		}
+
+		@Override
+		public synchronized void write(int b) throws IOException {
+			_outputStream1.write(b);
+			_outputStream2.write(b);
+		}
+
+		private final OutputStream _outputStream1;
+		private final OutputStream _outputStream2;
+
+	}
+}
