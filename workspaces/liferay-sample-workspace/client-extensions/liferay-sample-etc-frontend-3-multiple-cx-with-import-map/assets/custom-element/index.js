d41337821cf2519e08ce38a33e92e4a15c5592d5
drewbrokke <drew.brokke@liferay.com>
2023-09-11 13:54:05 -0500

    LPS-186240 liferay-sample-workspace: renames liferay-sample-etc-frontend-3 to liferay-sample-etc-frontend-3-multiple-cx-with-import-map

--- workspaces/liferay-sample-workspace/client-extensions/liferay-sample-etc-frontend-3-multiple-cx-with-import-map/assets/custom-element/index.js
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-etc-frontend-3-multiple-cx-with-import-map/assets/custom-element/index.js
@@ -46,9 +46,9 @@ Farewell in:
 	}
 }
 
-if (!customElements.get('liferay-sample-etc-frontend-3-custom-element')) {
+if (!customElements.get('liferay-sample-etc-frontend-3-multiple-cx-with-import-map-custom-element')) {
 	customElements.define(
-		'liferay-sample-etc-frontend-3-custom-element',
+		'liferay-sample-etc-frontend-3-multiple-cx-with-import-map-custom-element',
 		CustomElement
 	);
 }
