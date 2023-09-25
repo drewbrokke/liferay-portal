414d5227bd3c91d2497677d4aa63eb4e6f0f1fcc
drewbrokke <drew.brokke@liferay.com>
2023-09-25 11:28:43 -0500

    LPS-196641 feature-flag-web: adds FeatureFlagListenerRegistrar.java
    Responsibilities:
    
    - Tracking FeatureFlagListener services
    - Registering each service to FeatureFlagListenerRegistry under each of its
      declared featureFlagKey properties
    - Unregistering each service from the Registry when the service itself is
      unregistered from OSGi (using the SafeCloseables)
    - Logging an error when no featureFlagKey properties are found. The service will
      not be added to the registry.
    - Invokes the initial callback to the listener for each company for each
      declared featureFlagKey property.
    
    Q: Why is this class needed? Why not just track everything in
    FeatureFlagListenerRegistry?
    
    A: It would usually be much simpler to just use
    ServiceTrackerMapFactory.openMultiValueMap to track this kind of keyed service.
    However, since we also need to fire the callback when the service is first
    registered and we need to use the FeatureFlagManager service to do it, we risked
    having a circular dependency chain. Assume we put it all inside of
    FeatureFlagListenerRegistry, then the dependencies would look like:
    
    FeatureFlagListenerRegistry ->
    FeatureFlagManager(Impl) ->
    FeatureFlagsBagProvider ->
    FeatureFlagListenerRegistry ->
    FeatureFlagManager(Impl) ->
    FeatureFlagsBagProvider ->
    etc...
    
    Perhaps there's still a way to do it, but separating the registration and
    callback responsibilities allows us to keep the dependencies separate as well.

---
+++ modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/feature/flag/FeatureFlagListenerRegistrar.java
@@ -0,0 +1,156 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.feature.flag.web.internal.feature.flag;
+
+import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceMapper;
+import com.liferay.osgi.util.ServiceTrackerFactory;
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.feature.flag.FeatureFlag;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagListener;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManager;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.CompanyConstants;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(service = {})
+public class FeatureFlagListenerRegistrar
+	implements ServiceTrackerCustomizer
+		<FeatureFlagListener, List<SafeCloseable>> {
+
+	@Override
+	public List<SafeCloseable> addingService(
+		ServiceReference<FeatureFlagListener> serviceReference) {
+
+		FeatureFlagListener featureFlagListener = _bundleContext.getService(
+			serviceReference);
+
+		List<String> featureFlagKeys = new ArrayList<>();
+
+		_propertyServiceReferenceMapper.map(
+			serviceReference,
+			key -> {
+				if (key != null) {
+					featureFlagKeys.add(key);
+				}
+			});
+
+		if (featureFlagKeys.isEmpty()) {
+			Class<? extends FeatureFlagListener> clazz =
+				featureFlagListener.getClass();
+
+			_log.error(
+				StringBundler.concat(
+					"No featureFlagKey property found on the feature flag ",
+					"listener ", clazz.getName(), ". Skipping registration."));
+
+			return null;
+		}
+
+		List<SafeCloseable> safeCloseables = new ArrayList<>();
+
+		Predicate<FeatureFlag> featureFlagPredicate =
+			featureFlag -> featureFlagKeys.contains(featureFlag.getKey());
+
+		for (String featureFlagKey : featureFlagKeys) {
+			safeCloseables.add(
+				_featureFlagListenerRegistry.registerFeatureFlagListener(
+					featureFlagKey, featureFlagListener));
+		}
+
+		Predicate<FeatureFlag> finalFeatureFlagPredicate = featureFlagPredicate;
+
+		_companyLocalService.forEachCompanyId(
+			companyId -> {
+				for (FeatureFlag featureFlag :
+						_featureFlagManager.getFeatureFlags(
+							companyId, finalFeatureFlagPredicate)) {
+
+					featureFlagListener.onValue(
+						companyId, featureFlag.getKey(),
+						featureFlag.isEnabled());
+				}
+			});
+
+		for (FeatureFlag featureFlag :
+				_featureFlagManager.getFeatureFlags(
+					CompanyConstants.SYSTEM, finalFeatureFlagPredicate)) {
+
+			featureFlagListener.onValue(
+				CompanyConstants.SYSTEM, featureFlag.getKey(),
+				featureFlag.isEnabled());
+		}
+
+		return safeCloseables;
+	}
+
+	@Override
+	public void modifiedService(
+		ServiceReference<FeatureFlagListener> serviceReference,
+		List<SafeCloseable> safeCloseables) {
+	}
+
+	@Override
+	public void removedService(
+		ServiceReference<FeatureFlagListener> serviceReference,
+		List<SafeCloseable> safeCloseables) {
+
+		for (SafeCloseable safeCloseable : safeCloseables) {
+			safeCloseable.close();
+		}
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundleContext = bundleContext;
+
+		_serviceTracker = ServiceTrackerFactory.open(
+			_bundleContext, FeatureFlagListener.class, this);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTracker.close();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		FeatureFlagListenerRegistrar.class);
+
+	private BundleContext _bundleContext;
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private FeatureFlagListenerRegistry _featureFlagListenerRegistry;
+
+	@Reference
+	private FeatureFlagManager _featureFlagManager;
+
+	private final PropertyServiceReferenceMapper<String, FeatureFlagListener>
+		_propertyServiceReferenceMapper = new PropertyServiceReferenceMapper<>(
+			"featureFlagKey");
+	private ServiceTracker<FeatureFlagListener, List<SafeCloseable>>
+		_serviceTracker;
+
+}
