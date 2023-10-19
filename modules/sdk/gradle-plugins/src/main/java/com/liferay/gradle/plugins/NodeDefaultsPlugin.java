3524b6b36a4ba9d6534e3bb4208d6cd469ec571c
Simon Jiang <simon.jiang@liferay.com>
2023-10-19 10:50:26 +0800

    LPS-193787 dynamic get latest node and npm version

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/NodeDefaultsPlugin.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/NodeDefaultsPlugin.java
@@ -5,23 +5,81 @@
 
 package com.liferay.gradle.plugins;
 
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
 import com.liferay.gradle.plugins.internal.util.GradleUtil;
 import com.liferay.gradle.plugins.node.NodeExtension;
 import com.liferay.gradle.plugins.node.NodePlugin;
 import com.liferay.gradle.plugins.node.task.ExecutePackageManagerTask;
 import com.liferay.gradle.plugins.node.task.NpmInstallTask;
 import com.liferay.gradle.plugins.node.task.PublishNodeModuleTask;
+import com.liferay.gradle.util.StringUtil;
 import com.liferay.gradle.util.Validator;
+import com.liferay.portal.tools.bundle.support.commands.DownloadCommand;
+
+import java.io.File;
+
+import java.net.URL;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.gradle.api.Action;
+import org.gradle.api.GradleException;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.ExtensionContainer;
 import org.gradle.api.tasks.TaskContainer;
+
+import org.osgi.framework.Version;
+import org.parboiled.common.StringUtils;
 
 /**
  * @author Andrea Di Giorgi
  */
 public class NodeDefaultsPlugin extends BaseDefaultsPlugin<NodePlugin> {
+
+	public static class NodeInfo {
+
+		public String getDate() {
+			return _date;
+		}
+
+		public String getNpmVersion() {
+			return _npm;
+		}
+
+		public String getVersion() {
+			return _version;
+		}
+
+		public String isLtsVersion() {
+			return _lts;
+		}
+
+		@SerializedName("date")
+		private String _date;
+
+		@SerializedName("lts")
+		private String _lts;
+
+		@SerializedName("npm")
+		private String _npm;
+
+		@SerializedName("version")
+		private String _version;
+
+	}
 
 	@Override
 	protected void applyPluginDefaults(Project project, NodePlugin nodePlugin) {
@@ -32,6 +90,15 @@ public class NodeDefaultsPlugin extends BaseDefaultsPlugin<NodePlugin> {
 
 		NodeExtension nodeExtension = extensionContainer.getByType(
 			NodeExtension.class);
+
+		Optional<NodeInfo> nodeInfoOptional = _getNodeVersionInfo();
+
+		if (nodeInfoOptional.isPresent()) {
+			NodeInfo nodeInfo = nodeInfoOptional.get();
+
+			_nodeVersion = StringUtils.substring(nodeInfo.getVersion(), 1);
+			_npmVersion = nodeInfo.getNpmVersion();
+		}
 
 		_configureExtensionNode(project, nodeExtension);
 
@@ -87,8 +154,8 @@ public class NodeDefaultsPlugin extends BaseDefaultsPlugin<NodePlugin> {
 		Project project, NodeExtension nodeExtension) {
 
 		nodeExtension.setGlobal(true);
-		nodeExtension.setNodeVersion(_NODE_VERSION);
-		nodeExtension.setNpmVersion(_NPM_VERSION);
+		nodeExtension.setNodeVersion(_nodeVersion);
+		nodeExtension.setNpmVersion(_npmVersion);
 
 		String npmArgs = GradleUtil.getProperty(
 			project, "nodejs.npm.args", (String)null);
@@ -181,10 +248,70 @@ public class NodeDefaultsPlugin extends BaseDefaultsPlugin<NodePlugin> {
 		}
 	}
 
-	private static final String _NODE_VERSION = "16.13.0";
+	private Optional<NodeInfo> _getNodeInfos(Path downloadPath)
+		throws Exception {
 
-	private static final String _NPM_VERSION = "8.1.0";
+		try (JsonReader jsonReader = new JsonReader(
+				Files.newBufferedReader(downloadPath))) {
+
+			List<NodeInfo> nodeInfos = _parserNodeInfos(jsonReader);
+
+			return nodeInfos.stream(
+			).filter(
+				nodeInfo -> !Objects.equals(nodeInfo.isLtsVersion(), "false")
+			).min((first, second) -> {
+				Version firstVersion = Version.parseVersion(
+					StringUtils.substring(first.getVersion(), 1));
+				Version secondVersion = Version.parseVersion(
+					StringUtils.substring(second.getVersion(), 1));
+
+				return -1 * firstVersion.compareTo(secondVersion);
+			});
+		}
+	}
+
+	private Optional<NodeInfo> _getNodeVersionInfo() {
+		DownloadCommand downloadCommand = new DownloadCommand();
+
+		downloadCommand.setCacheDir(_nodeCacheDir);
+		downloadCommand.setConnectionTimeout(5 * 1000);
+		downloadCommand.setPassword(null);
+		downloadCommand.setQuiet(true);
+		downloadCommand.setToken(false);
+		downloadCommand.setUserName(null);
+
+		try {
+			downloadCommand.setUrl(new URL(_PRODUCT_NODE_URL));
+
+			downloadCommand.execute();
+
+			return _getNodeInfos(downloadCommand.getDownloadPath());
+		}
+		catch (Exception exception) {
+			throw new GradleException(
+				"Unable to get node version", exception.getCause());
+		}
+	}
+
+	private List<NodeInfo> _parserNodeInfos(JsonReader jsonReader) {
+		Gson gson = new Gson();
+
+		TypeToken<List<NodeInfo>> typeToken = new TypeToken<List<NodeInfo>>() {
+		};
+
+		return gson.fromJson(jsonReader, typeToken.getType());
+	}
+
+	private static final String _DEFAULT_NODE_CACHE_DIR_NAME = ".liferay/node";
+
+	private static final String _PRODUCT_NODE_URL =
+		"https://nodejs.org/dist/index.json";
 
 	private static final String _SASS_BINARY_SITE_ARG = "--sass-binary-site=";
 
+	private static String _nodeVersion = "16.13.0";
+	private static String _npmVersion = "8.1.0";
+
+	private final File _nodeCacheDir = new File(
+		System.getProperty("user.home"), _DEFAULT_NODE_CACHE_DIR_NAME);
 }
