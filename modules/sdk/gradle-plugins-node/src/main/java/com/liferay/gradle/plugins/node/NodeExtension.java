50bfffd4bad369ce6bdcf5d863cffb5f9b5acfe8
Simon Jiang <simon.jiang@liferay.com>
2023-10-23 11:27:57 +0800

    LPS-193787 add useLatestNode property

--- modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/NodeExtension.java
+++ modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/NodeExtension.java
@@ -243,6 +243,10 @@ public class NodeExtension {
 		return _global;
 	}
 
+	public boolean isUselatestNode() {
+		return GradleUtil.toBoolean(_useLatestNode);
+	}
+
 	public boolean isUseNpm() {
 		return GradleUtil.toBoolean(_useNpm);
 	}
@@ -299,6 +303,10 @@ public class NodeExtension {
 		_scriptFile = scriptFile;
 	}
 
+	public void setUseLatestNode(Object useLatestNode) {
+		_useLatestNode = useLatestNode;
+	}
+
 	public void setUseNpm(Object useNpm) {
 		_useNpm = useNpm;
 	}
@@ -342,6 +350,7 @@ public class NodeExtension {
 	private Object _npmVersion;
 	private final Project _project;
 	private Object _scriptFile;
+	private Object _useLatestNode = false;
 	private Object _useNpm = true;
 	private Object _yarnUrl;
 	private Object _yarnVersion = "1.13.0";
