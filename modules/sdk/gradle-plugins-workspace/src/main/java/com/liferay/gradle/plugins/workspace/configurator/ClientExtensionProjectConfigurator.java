4f52c57666b424f68277824edd97a44878001e75
drewbrokke <drew.brokke@liferay.com>
2023-10-25 12:54:35 -0500

    LPS-183167 gradle-plugins-workspace: no lambda

--- modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurator/ClientExtensionProjectConfigurator.java
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurator/ClientExtensionProjectConfigurator.java
@@ -707,42 +707,47 @@ public class ClientExtensionProjectConfigurator
 				outputs.upToDateWhen(task -> true);
 
 				validateClientExtensionIdsTask.doFirst(
-					validateClientExtensionIdsTask1 -> {
-						StringBundler sb = new StringBundler();
+					new Action<Task>() {
+						@Override
+						public void execute(
+							Task validateClientExtensionIdsTask1) {
+							StringBundler sb = new StringBundler();
 
-						File rootDir = project.getRootDir();
+							File rootDir = project.getRootDir();
 
-						Path rootDirPath = rootDir.toPath();
+							Path rootDirPath = rootDir.toPath();
 
-						for (Map.Entry<String, Set<Project>> entry :
+							for (Map.Entry<String, Set<Project>> entry :
 								_clientExtensionIds.entrySet()) {
 
-							Set<Project> projects = entry.getValue();
+								Set<Project> projects = entry.getValue();
 
-							if ((projects.size() > 1) &&
-								projects.contains(project)) {
-
-								sb.append("Duplicate client extension ID \"");
-								sb.append(entry.getKey());
-								sb.append("\" found in these projects:\n");
-
-								for (Project curProject : projects) {
-									File projectDir =
-										curProject.getProjectDir();
+								if ((projects.size() > 1) &&
+									projects.contains(project)) {
 
 									sb.append(
-										rootDirPath.relativize(
-											projectDir.toPath()));
+										"Duplicate client extension ID \"");
+									sb.append(entry.getKey());
+									sb.append("\" found in these projects:\n");
+
+									for (Project curProject : projects) {
+										File projectDir =
+											curProject.getProjectDir();
+
+										sb.append(
+											rootDirPath.relativize(
+												projectDir.toPath()));
+
+										sb.append(StringPool.NEW_LINE);
+									}
 
 									sb.append(StringPool.NEW_LINE);
 								}
-
-								sb.append(StringPool.NEW_LINE);
 							}
-						}
 
-						if (sb.length() > 0) {
-							throw new GradleException(sb.toString());
+							if (sb.length() > 0) {
+								throw new GradleException(sb.toString());
+							}
 						}
 					});
 			});
