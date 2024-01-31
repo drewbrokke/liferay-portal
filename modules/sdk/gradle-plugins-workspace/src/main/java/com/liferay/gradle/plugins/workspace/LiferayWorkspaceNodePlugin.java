8d15afa57c6b9b47214601f35ba090bbf4f91d11
drewbrokke <drew.brokke@liferay.com>
2024-01-31 17:57:10 -0600

    LPD-16079 gradle-plugins-workspace: apply static util to LiferayWorkspaceNodePlugin

--- modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/LiferayWorkspaceNodePlugin.java
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/LiferayWorkspaceNodePlugin.java
@@ -7,31 +7,22 @@ package com.liferay.gradle.plugins.workspace;
 
 import aQute.bnd.version.Version;
 
-import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
 
 import com.liferay.gradle.plugins.node.NodeExtension;
 import com.liferay.gradle.plugins.node.NodePlugin;
 import com.liferay.gradle.plugins.workspace.internal.util.GradleUtil;
+import com.liferay.gradle.plugins.workspace.internal.util.ResourceUtil;
 import com.liferay.gradle.plugins.workspace.internal.util.StringUtil;
 import com.liferay.gradle.util.Validator;
-import com.liferay.portal.tools.bundle.support.commands.DownloadCommand;
 
 import java.io.File;
-import java.io.IOException;
 
-import java.net.URL;
-
-import java.nio.file.Files;
-
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.gradle.api.GradleException;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.logging.Logger;
@@ -56,42 +47,10 @@ public class LiferayWorkspaceNodePlugin implements Plugin<Project> {
 	}
 
 	private LiferayWorkspaceNodePlugin() {
-		DownloadCommand downloadCommand = new DownloadCommand();
-
-		downloadCommand.setCacheDir(
-			new File(
-				System.getProperty("user.home"), _DEFAULT_NODE_CACHE_DIR_NAME));
-		downloadCommand.setConnectionTimeout(5 * 1000);
-		downloadCommand.setPassword(null);
-		downloadCommand.setQuiet(true);
-		downloadCommand.setToken(false);
-		downloadCommand.setUserName(null);
-
-		try {
-			downloadCommand.setUrl(new URL(_PRODUCT_NODE_URL));
-
-			downloadCommand.execute();
-		}
-		catch (Exception exception) {
-			throw new GradleException(
-				"Unable to get node version", exception.getCause());
-		}
-
-		try (JsonReader jsonReader = new JsonReader(
-				Files.newBufferedReader(downloadCommand.getDownloadPath()))) {
-
-			Gson gson = new Gson();
-
-			TypeToken<List<NodeInfo>> typeToken =
-				new TypeToken<List<NodeInfo>>() {
-				};
-
-			_nodeInfos = gson.fromJson(jsonReader, typeToken.getType());
-		}
-		catch (IOException ioException) {
-			throw new GradleException(
-				"Unable to read downloaded file", ioException.getCause());
-		}
+		_nodeInfos = ResourceUtil.readJson(
+			NodeInfos.class,
+			ResourceUtil.getURLResolver(_NODE_CACHE_DIR, _PRODUCT_NODE_URL),
+			ResourceUtil.getClassLoaderResolver("/.node_info.json"));
 	}
 
 	private void _configureLTS(Project project) {
@@ -175,10 +134,13 @@ public class LiferayWorkspaceNodePlugin implements Plugin<Project> {
 
 	private static final String _DEFAULT_NODE_CACHE_DIR_NAME = ".liferay/node";
 
+	private static final File _NODE_CACHE_DIR = new File(
+		System.getProperty("user.home"), _DEFAULT_NODE_CACHE_DIR_NAME);
+
 	private static final String _PRODUCT_NODE_URL =
 		"https://nodejs.org/dist/index.json";
 
-	private final List<NodeInfo> _nodeInfos;
+	private final NodeInfos _nodeInfos;
 
 	private static class NodeInfo {
 
@@ -203,6 +165,9 @@ public class LiferayWorkspaceNodePlugin implements Plugin<Project> {
 		@SerializedName("npm")
 		private String _npmVersion;
 
+	}
+
+	private static class NodeInfos extends ArrayList<NodeInfo> {
 	}
 
 }
