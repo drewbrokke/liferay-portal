5c404c7fb5d43b3c7ffa1580325bdb9a759aa728
Drew Brokke <drew.brokke@liferay.com>
2024-12-12 09:10:24 -0600

    LPD-44208 blogs: javax.servlet to jakarta.servlet for java files

--- modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/service/impl/BlogsEntryLocalServiceImpl.java
+++ modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/service/impl/BlogsEntryLocalServiceImpl.java
@@ -129,7 +129,7 @@ import java.util.Set;
 
 import javax.portlet.PortletRequest;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 import net.htmlparser.jericho.Source;
 import net.htmlparser.jericho.StartTag;
