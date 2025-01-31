c4a2485e2fd4baefd25f6ad9a3c16fe5ba29f492
Drew Brokke <drew.brokke@liferay.com>
2025-01-31 12:11:14 -0600

    LPS-00000 source-formatter: fail on purpose

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/SourceFormatter.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/SourceFormatter.java
@@ -107,6 +107,12 @@ public class SourceFormatter {
 	public static void main(String[] args) throws Exception {
 		Map<String, String> arguments = ArgumentsUtil.parseArguments(args);
 
+		boolean fail = true;
+
+		if (fail) {
+			throw new RuntimeException("Fail on purpose");
+		}
+
 		try {
 			SourceFormatterArgs sourceFormatterArgs = new SourceFormatterArgs();
 
