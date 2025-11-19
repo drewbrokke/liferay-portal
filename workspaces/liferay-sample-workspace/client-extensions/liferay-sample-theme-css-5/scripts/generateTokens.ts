f1b6a5a46fb8e1f2b41898497eee82b67f7b0fe8
Drew Brokke <drew.brokke@liferay.com>
2025-11-19 16:46:32 +0100

    LPS-00000 restore scss path

--- workspaces/liferay-sample-workspace/client-extensions/liferay-sample-theme-css-5/scripts/generateTokens.ts
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-theme-css-5/scripts/generateTokens.ts
@@ -7,13 +7,9 @@ import * as fs from 'fs/promises';
 import * as path from 'path';
 
 const INPUT_PATH = path.resolve('./lexicon-foundations-tokens.json');
-// const OUTPUT_PATH = path.resolve('./src/frontend-token-definition.json');
 const OUTPUT_PATH = path.resolve('./build/generateTokens/frontend-token-definition.json');
-// const OUTPUT_PATH_SASS = path.resolve(
-// 	'./src/css/_lexicon-foundations-tokens.scss'
-// );
 const OUTPUT_PATH_SASS = path.resolve(
-	'./build/generateTokens/_lexicon-foundations-tokens.scss'
+	'./src/css/_lexicon-foundations-tokens.scss'
 );
 
 type IFigmaToken = {
