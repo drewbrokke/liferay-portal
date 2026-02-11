e59e12cb5863394bce9e4a5a41f04c495f55881e
Drew Brokke <drew.brokke@liferay.com>
2026-02-11 07:24:30 -0600

    LPD-79061 feature-flag-web: no need to guard this statement now since the cluster request will only invoke the private method

--- modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/feature/flag/FeatureFlagsBagProviderImpl.java
+++ modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/feature/flag/FeatureFlagsBagProviderImpl.java
@@ -19,7 +19,6 @@ import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.cluster.ClusterExecutor;
-import com.liferay.portal.kernel.cluster.ClusterInvokeThreadLocal;
 import com.liferay.portal.kernel.cluster.ClusterRequest;
 import com.liferay.portal.kernel.feature.flag.FeatureFlag;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagListener;
@@ -100,9 +99,7 @@ public class FeatureFlagsBagProviderImpl implements FeatureFlagsBagProvider {
 
 	@Override
 	public void setEnabled(long companyId, String key, boolean enabled) {
-		if (ClusterInvokeThreadLocal.isEnabled()) {
-			_featureFlagPreferencesManager.setEnabled(companyId, key, enabled);
-		}
+		_featureFlagPreferencesManager.setEnabled(companyId, key, enabled);
 
 		_setEnabled(companyId, key, enabled);
 
