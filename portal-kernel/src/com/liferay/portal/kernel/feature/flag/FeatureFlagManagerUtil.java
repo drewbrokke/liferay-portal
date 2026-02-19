dacf46028dce2229a04b11cb04e9536501b189af
Drew Brokke <drew.brokke@liferay.com>
2026-02-19 16:36:43 -0600

    LPD-79062 portal-kernel: FeatureFlagManagerUtil: adds helper methods for use by FeatureFlagTestRule

--- portal-kernel/src/com/liferay/portal/kernel/feature/flag/FeatureFlagManagerUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/feature/flag/FeatureFlagManagerUtil.java
@@ -42,6 +42,10 @@ public class FeatureFlagManagerUtil {
 		}
 	}
 
+	public static FeatureFlagManager getFeatureFlagManager() {
+		return _featureFlagManager;
+	}
+
 	public static String getJSON(long companyId) {
 		if (_featureFlagManager != null) {
 			return _featureFlagManager.getJSON(companyId);
@@ -50,7 +54,7 @@ public class FeatureFlagManagerUtil {
 		if (_log.isInfoEnabled()) {
 			_log.info(
 				"No feature flag manager service found. Returning the " +
-					"default value.");
+				"default value.");
 		}
 
 		if (_json == null) {
@@ -70,7 +74,7 @@ public class FeatureFlagManagerUtil {
 		if (_log.isInfoEnabled()) {
 			_log.info(
 				"No feature flag manager service found. Returning the " +
-					"default value.");
+				"default value.");
 		}
 
 		return GetterUtil.getBoolean(PropsUtil.get("feature.flag." + key));
@@ -93,6 +97,16 @@ public class FeatureFlagManagerUtil {
 		return new FeatureFlaggedServiceRegistration<>(
 			bundleContext, featureFlagKey, serviceClass, serviceFunction,
 			servicePropertiesFunction);
+	}
+
+	public static AutoCloseable setFeatureFlagManagerWithAutoCloseable(
+		FeatureFlagManager featureFlagManager) {
+
+		FeatureFlagManager oldFeatureFlagManager = _featureFlagManager;
+
+		_featureFlagManager = featureFlagManager;
+
+		return () -> _featureFlagManager = oldFeatureFlagManager;
 	}
 
 	public void setFeatureFlagManager(FeatureFlagManager featureFlagManager) {
