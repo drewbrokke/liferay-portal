c143b2ae7334f89127e88cc9eb9ddd1a5e554c00
Drew Brokke <drew.brokke@liferay.com>
2026-02-25 14:07:07 -0600

    LPS-00000 depot-service: inline logic to avoid the feature flag check inside of a feature flag listener

--- modules/apps/depot/depot-service/src/main/java/com/liferay/depot/internal/feature/flag/CMSFeatureFlagListener.java
+++ modules/apps/depot/depot-service/src/main/java/com/liferay/depot/internal/feature/flag/CMSFeatureFlagListener.java
@@ -5,15 +5,23 @@
 
 package com.liferay.depot.internal.feature.flag;
 
+import com.liferay.depot.constants.DepotRolesConstants;
+import com.liferay.depot.internal.instance.lifecycle.DepotRolesPortalInstanceLifecycleListener;
 import com.liferay.depot.internal.util.DepotRoleUtil;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagListener;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.Validator;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -41,8 +49,7 @@ public class CMSFeatureFlagListener implements FeatureFlagListener {
 				continue;
 			}
 
-			Map<Locale, String> titleMap = DepotRoleUtil.getTitleMap(
-				companyId, _language, name);
+			Map<Locale, String> titleMap = _getTitleMap(_language, name);
 
 			if (Objects.equals(titleMap, role.getTitleMap())) {
 				continue;
@@ -52,6 +59,53 @@ public class CMSFeatureFlagListener implements FeatureFlagListener {
 
 			_roleLocalService.updateRole(role);
 		}
+	}
+
+	private String _getTitle(Locale locale, String name) {
+		String title = _titleKeys.get(name);
+
+		if (Validator.isNull(title)) {
+			return name;
+		}
+
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			locale, DepotRolesPortalInstanceLifecycleListener.class);
+
+		return ResourceBundleUtil.getString(resourceBundle, title);
+	}
+
+	private Map<Locale, String> _getTitleMap(Language language, String name) {
+		Map<Locale, String> titleMap = new HashMap<>();
+
+		for (Locale locale : language.getAvailableLocales()) {
+			String title = _getTitle(locale, name);
+
+			if (title != null) {
+				titleMap.put(locale, title);
+			}
+		}
+
+		return titleMap;
+	}
+
+	private static final Map<String, String> _titleKeys;
+
+	static {
+		_titleKeys = Collections.unmodifiableMap(
+			HashMapBuilder.put(
+				DepotRolesConstants.ASSET_LIBRARY_ADMINISTRATOR,
+				"space-administrator"
+			).put(
+				DepotRolesConstants.ASSET_LIBRARY_CONNECTED_SITE_MEMBER,
+				"space-connected-site-member"
+			).put(
+				DepotRolesConstants.ASSET_LIBRARY_CONTENT_REVIEWER,
+				"space-content-reviewer"
+			).put(
+				DepotRolesConstants.ASSET_LIBRARY_MEMBER, "space-member"
+			).put(
+				DepotRolesConstants.ASSET_LIBRARY_OWNER, "space-owner"
+			).build());
 	}
 
 	@Reference
