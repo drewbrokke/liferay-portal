7ccf264f10b61630bb713cf2c2d7c05ee1d12981
Drew Brokke <drew.brokke@liferay.com>
2026-06-30 10:59:54 -0500

    LPD-90495 adds conversion from Account DTO to JiraAssetObject to AccountConverter

--- workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/jira/converter/AccountConverter.java
+++ workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/jira/converter/AccountConverter.java
@@ -5,8 +5,22 @@
 
 package com.liferay.one.jira.converter;
 
+import com.liferay.headless.admin.user.client.custom.field.CustomField;
+import com.liferay.headless.admin.user.client.custom.field.CustomValue;
+import com.liferay.headless.admin.user.client.dto.v1_0.Account;
+import com.liferay.headless.admin.user.client.dto.v1_0.AccountContactInformation;
+import com.liferay.headless.admin.user.client.dto.v1_0.EmailAddress;
+import com.liferay.headless.admin.user.client.dto.v1_0.Phone;
+import com.liferay.headless.admin.user.client.dto.v1_0.WebUrl;
+import com.liferay.one.jira.client.JiraAssetObject;
 import com.liferay.one.jira.constants.AccountConstants;
 import com.liferay.one.jira.model.JiraAssetSchemaNameProvider;
+import com.liferay.portal.kernel.util.ArrayUtil;
+
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,6 +31,56 @@ import org.springframework.stereotype.Component;
 @Component
 public class AccountConverter extends BaseAssetObjectConverter {
 
+	public JiraAssetObject toAssetObject(Account account) throws Exception {
+		JiraAssetObject jiraAssetObject = new JiraAssetObject(
+			getAttributeIds());
+
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_EXTERNAL_KEY,
+			account.getExternalReferenceCode());
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_NAME, account.getName());
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_DESCRIPTION,
+			account.getDescription());
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_EXTERNAL_CREATED_AT,
+			_format(account.getDateCreated()));
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_EXTERNAL_UPDATED_AT,
+			_format(account.getDateModified()));
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_STATUS,
+			_status(account.getStatus()));
+
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_CODE,
+			_customFieldData(account, _CUSTOM_FIELD_NAME_ACCOUNT_CODE));
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_TIER,
+			_customFieldData(account, _CUSTOM_FIELD_NAME_ACCOUNT_TIER));
+
+		AccountContactInformation accountContactInformation =
+			account.getAccountContactInformation();
+
+		if (accountContactInformation != null) {
+			jiraAssetObject.setAttributeValue(
+				AccountConstants.ATTRIBUTE_NAME_CONTACT_EMAIL_ADDRESS,
+				_getEmailAddress(accountContactInformation));
+			jiraAssetObject.setAttributeValue(
+				AccountConstants.ATTRIBUTE_NAME_FAX_NUMBER,
+				_getFaxNumber(accountContactInformation));
+			jiraAssetObject.setAttributeValue(
+				AccountConstants.ATTRIBUTE_NAME_PHONE_NUMBER,
+				_getPhoneNumber(accountContactInformation));
+			jiraAssetObject.setAttributeValue(
+				AccountConstants.ATTRIBUTE_NAME_WEBSITE,
+				_getWebsite(accountContactInformation));
+		}
+
+		return jiraAssetObject;
+	}
+
 	@Override
 	protected String getObjectSchemaName() {
 		return _jiraAssetSchemaNameProvider.getSchemaName();
@@ -26,6 +90,151 @@ public class AccountConverter extends BaseAssetObjectConverter {
 	protected String getObjectTypeName() {
 		return AccountConstants.OBJECT_TYPE_NAME;
 	}
+
+	private Object _customFieldData(Account account, String name) {
+		CustomField[] customFields = account.getCustomFields();
+
+		if (customFields == null) {
+			return null;
+		}
+
+		for (CustomField customField : customFields) {
+			if (!name.equals(customField.getName())) {
+				continue;
+			}
+
+			CustomValue customValue = customField.getCustomValue();
+
+			if (customValue == null) {
+				return null;
+			}
+
+			return customValue.getData();
+		}
+
+		return null;
+	}
+
+	private String _format(Date date) {
+		if (date == null) {
+			return null;
+		}
+
+		SimpleDateFormat simpleDateFormat = _simpleDateFormat();
+
+		return simpleDateFormat.format(date);
+	}
+
+	private String _getEmailAddress(
+		AccountContactInformation accountContactInformation) {
+
+		EmailAddress[] emailAddresses =
+			accountContactInformation.getEmailAddresses();
+
+		if (ArrayUtil.isEmpty(emailAddresses)) {
+			return null;
+		}
+
+		for (EmailAddress emailAddress : emailAddresses) {
+			if (Boolean.TRUE.equals(emailAddress.getPrimary())) {
+				return emailAddress.getEmailAddress();
+			}
+		}
+
+		EmailAddress emailAddress = emailAddresses[0];
+
+		return emailAddress.getEmailAddress();
+	}
+
+	private String _getFaxNumber(
+		AccountContactInformation accountContactInformation) {
+
+		Phone[] telephones = accountContactInformation.getTelephones();
+
+		if (telephones == null) {
+			return null;
+		}
+
+		for (Phone telephone : telephones) {
+			if ("fax".equalsIgnoreCase(telephone.getPhoneType())) {
+				return telephone.getPhoneNumber();
+			}
+		}
+
+		return null;
+	}
+
+	private String _getPhoneNumber(
+		AccountContactInformation accountContactInformation) {
+
+		Phone[] telephones = accountContactInformation.getTelephones();
+
+		if (ArrayUtil.isEmpty(telephones)) {
+			return null;
+		}
+
+		for (Phone telephone : telephones) {
+			if ("fax".equalsIgnoreCase(telephone.getPhoneType())) {
+				continue;
+			}
+
+			if (Boolean.TRUE.equals(telephone.getPrimary())) {
+				return telephone.getPhoneNumber();
+			}
+		}
+
+		Phone telephone = telephones[0];
+
+		return telephone.getPhoneNumber();
+	}
+
+	private String _getWebsite(
+		AccountContactInformation accountContactInformation) {
+
+		WebUrl[] webUrls = accountContactInformation.getWebUrls();
+
+		if (ArrayUtil.isEmpty(webUrls)) {
+			return null;
+		}
+
+		for (WebUrl webUrl : webUrls) {
+			if (Boolean.TRUE.equals(webUrl.getPrimary())) {
+				return webUrl.getUrl();
+			}
+		}
+
+		WebUrl webUrl = webUrls[0];
+
+		return webUrl.getUrl();
+	}
+
+	private SimpleDateFormat _simpleDateFormat() {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+			"yyyy-MM-dd'T'HH:mm:ssXX");
+
+		simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+		return simpleDateFormat;
+	}
+
+	private String _status(Integer status) {
+
+		// Best-guess mapping; adjust to the real Liferay account status codes.
+
+		if (status == null) {
+			return null;
+		}
+
+		if (status == 0) {
+			return "Active";
+		}
+
+		return "Closed";
+	}
+
+	private static final String _CUSTOM_FIELD_NAME_ACCOUNT_CODE = "accountCode";
+
+	private static final String _CUSTOM_FIELD_NAME_ACCOUNT_TIER = "accountTier";
 
 	@Autowired
 	private JiraAssetSchemaNameProvider _jiraAssetSchemaNameProvider;
