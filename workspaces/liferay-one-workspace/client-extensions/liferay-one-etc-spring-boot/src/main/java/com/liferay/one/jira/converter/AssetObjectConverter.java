6fdb82bdd7368007516c5eedb80b41435cf0e9cf
Drew Brokke <drew.brokke@liferay.com>
2026-06-30 10:36:09 -0500

    LPD-90495 removes helper methods from AssetObjectConverter that are now handled by JiraAssetObject

--- workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/jira/converter/AssetObjectConverter.java
+++ workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/jira/converter/AssetObjectConverter.java
@@ -35,77 +35,9 @@ public abstract class AssetObjectConverter {
 			getObjectSchemaName(), getObjectTypeName());
 	}
 
-	protected String getAttributeKey(
-		String attributeId, JSONObject jsonObject) {
-
-		return _getAttributeKey(
-			_getAttributeValueJSONObject(
-				attributeId, jsonObject.getJSONArray("attributes")));
-	}
-
-	protected String getAttributeValue(
-		String attributeId, JSONObject jsonObject) {
-
-		JSONObject attributeValueJSONObject = _getAttributeValueJSONObject(
-			attributeId, jsonObject.getJSONArray("attributes"));
-
-		return attributeValueJSONObject.optString(
-			"displayValue", _getAttributeKey(attributeValueJSONObject));
-	}
-
 	protected abstract String getObjectSchemaName();
 
 	protected abstract String getObjectTypeName();
-
-	private String _getAttributeKey(JSONObject attributeValueJSONObject) {
-		String key = attributeValueJSONObject.optString("value");
-
-		if (Validator.isNull(key)) {
-			JSONObject referencedObjectJSONObject =
-				attributeValueJSONObject.optJSONObject("referencedObject");
-
-			if (referencedObjectJSONObject != null) {
-				key = referencedObjectJSONObject.optString("id");
-			}
-		}
-
-		return key;
-	}
-
-	private JSONObject _getAttributeValueJSONObject(
-		String attributeId, JSONArray attributesJSONArray) {
-
-		if (Validator.isNull(attributeId)) {
-			return new JSONObject();
-		}
-
-		for (int i = 0; i < attributesJSONArray.length(); i++) {
-			JSONObject attributeJSONObject = attributesJSONArray.getJSONObject(
-				i);
-
-			String objectTypeAttributeId = attributeJSONObject.optString(
-				"objectTypeAttributeId");
-
-			if (Validator.isNull(objectTypeAttributeId) ||
-				!attributeId.equals(objectTypeAttributeId)) {
-
-				continue;
-			}
-
-			JSONArray attributeValuesJSONArray =
-				attributeJSONObject.optJSONArray("objectAttributeValues");
-
-			if ((attributeValuesJSONArray == null) ||
-				attributeValuesJSONArray.isEmpty()) {
-
-				break;
-			}
-
-			return attributeValuesJSONArray.getJSONObject(0);
-		}
-
-		return new JSONObject();
-	}
 
 	@Autowired
 	private AssetSchemaService _assetSchemaService;
