f13396d382c5baa0f023439f64d3e7311091c521
Drew Brokke <drew.brokke@liferay.com>
2026-06-30 13:38:31 -0500

    LPD-90495 adds BaseJiraService to share auth

---
+++ workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/jira/service/BaseJiraService.java
@@ -0,0 +1,42 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.one.jira.service;
+
+import com.liferay.client.extension.util.spring.boot3.service.BaseService;
+import com.liferay.petra.string.StringPool;
+
+import java.nio.charset.StandardCharsets;
+
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Drew Brokke
+ */
+@Component
+public class BaseJiraService extends BaseService {
+
+	protected String getAuthorization() {
+		Base64.Encoder encoder = Base64.getEncoder();
+
+		String credentials =
+			_jiraAPIEmailAddress + StringPool.COLON + _jiraAPIToken;
+
+		String encodedString = encoder.encodeToString(
+			credentials.getBytes(StandardCharsets.UTF_8));
+
+		return "Basic " + encodedString;
+	}
+
+	@Value("${liferay.one.jira.api.email.address}")
+	private String _jiraAPIEmailAddress;
+
+	@Value("${liferay.one.jira.api.token}")
+	private String _jiraAPIToken;
+
+}
