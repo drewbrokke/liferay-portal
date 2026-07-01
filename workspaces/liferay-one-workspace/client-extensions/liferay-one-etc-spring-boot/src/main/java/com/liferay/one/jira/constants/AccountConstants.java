b241a5b0efd4b9073f0582dfcb7e12e91df06c20
Drew Brokke <drew.brokke@liferay.com>
2026-07-01 12:29:49 -0500

    LPD-90495 adapt the Converter classes to now work with JiraAssetObject and add additional helper methods

---
+++ workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/jira/constants/AccountConstants.java
@@ -0,0 +1,17 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.one.jira.constants;
+
+/**
+ * @author Drew Brokke
+ */
+public interface AccountConstants {
+
+	public static final String ATTRIBUTE_NAME_EXTERNAL_KEY = "External Key";
+
+	public static final String OBJECT_TYPE_NAME = "Account";
+
+}
