54e1427fbcd7c036526fc18f06d767334a3ddc84
Drew Brokke <drew.brokke@liferay.com>
2026-07-01 12:22:48 -0500

    LPD-90495 adds JiraAssetService and supporting classes to handle transport and shared logic for Jira Assets work

--- workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/jira/service/AssetSchemaLoader.java
+++ workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/jira/service/AssetSchemaLoader.java
@@ -5,106 +5,50 @@
 
 package com.liferay.one.jira.service;
 
-import com.liferay.client.extension.util.spring.boot3.service.BaseService;
 import com.liferay.one.jira.exception.JiraAssetSchemaException;
-import com.liferay.petra.string.StringBundler;
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.Validator;
 
-import java.util.Base64;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
-import org.springframework.web.util.UriComponentsBuilder;
 
 /**
+ * Caches and resolves JSM Assets schema metadata to attribute name-to-id maps.
+ *
  * @author Drew Brokke
  */
 @Component
-public class AssetSchemaLoader extends BaseService {
+public class AssetSchemaLoader {
 
 	@Cacheable("assetObjectTypeAttributeIds")
 	public Map<String, String> getAttributeIds(String objectTypeId) {
 		return _toNameIdMap(
-			new JSONArray(
-				_get(
-					StringBundler.concat(
-						"objecttype/", objectTypeId, "/attributes"))));
+			_jiraAssetService.getObjectTypeAttributes(objectTypeId));
 	}
 
 	@Cacheable("assetObjectTypeIds")
 	public Map<String, String> getObjectTypeIds(String schemaName) {
 		String schemaId = _resolveSchemaId(schemaName);
 
-		return _toNameIdMap(
-			new JSONArray(
-				_get(
-					StringBundler.concat(
-						"objectschema/", schemaId, "/objecttypes"))));
-	}
-
-	private String _get(String path) {
-		try {
-			return get(
-				_getAuthorization(),
-				UriComponentsBuilder.fromUriString(
-					StringBundler.concat(
-						_JIRA_CLOUD_API_URL, "/jsm/assets/workspace/",
-						_jiraWorkspaceId, "/v1/", path)
-				).build(
-				).toUri());
-		}
-		catch (Exception exception) {
-			throw new JiraAssetSchemaException(
-				"Unable to complete JSM Assets request for " + path, exception);
-		}
-	}
-
-	private String _getAuthorization() {
-		Base64.Encoder encoder = Base64.getEncoder();
-
-		String credentials =
-			_jiraAPIEmailAddress + StringPool.COLON + _jiraAPIToken;
-
-		return "Basic " + encoder.encodeToString(credentials.getBytes());
+		return _toNameIdMap(_jiraAssetService.getObjectTypes(schemaId));
 	}
 
 	private String _resolveSchemaId(String schemaName) {
-		int startAt = 0;
+		JSONArray objectSchemasJSONArray = _jiraAssetService.getObjectSchemas();
 
-		while (true) {
-			JSONObject responseJSONObject = new JSONObject(
-				_get(
-					StringBundler.concat(
-						"objectschema/list?maxResults=", _MAX_RESULTS,
-						"&startAt=", startAt)));
+		for (int i = 0; i < objectSchemasJSONArray.length(); i++) {
+			JSONObject schemaJSONObject = objectSchemasJSONArray.getJSONObject(
+				i);
 
-			JSONArray valuesJSONArray = responseJSONObject.optJSONArray(
-				"values");
-
-			if ((valuesJSONArray == null) || valuesJSONArray.isEmpty()) {
-				break;
+			if (schemaName.equals(schemaJSONObject.optString("name"))) {
+				return schemaJSONObject.getString("id");
 			}
-
-			for (int i = 0; i < valuesJSONArray.length(); i++) {
-				JSONObject schemaJSONObject = valuesJSONArray.getJSONObject(i);
-
-				if (schemaName.equals(schemaJSONObject.optString("name"))) {
-					return schemaJSONObject.getString("id");
-				}
-			}
-
-			if (responseJSONObject.optBoolean("isLast")) {
-				break;
-			}
-
-			startAt += _MAX_RESULTS;
 		}
 
 		throw new JiraAssetSchemaException(
@@ -131,18 +75,7 @@ public class AssetSchemaLoader extends BaseService {
 		return attributeIds;
 	}
 
-	private static final String _JIRA_CLOUD_API_URL =
-		"https://api.atlassian.com";
-
-	private static final int _MAX_RESULTS = 100;
-
-	@Value("${liferay.one.jira.api.email.address}")
-	private String _jiraAPIEmailAddress;
-
-	@Value("${liferay.one.jira.api.token}")
-	private String _jiraAPIToken;
-
-	@Value("${liferay.one.jira.workspace.id}")
-	private String _jiraWorkspaceId;
+	@Autowired
+	private JiraAssetService _jiraAssetService;
 
 }
