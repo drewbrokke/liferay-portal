0c9fb4f47dabe08aa8f0f6acd8d86f0a5b7baa43
Drew Brokke <drew.brokke@liferay.com>
2026-07-01 22:50:33 -0500

    LPD-90495 prepare Account converter and services for sync

--- workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/jira/constants/AccountConstants.java
+++ workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/jira/constants/AccountConstants.java
@@ -10,8 +10,72 @@ package com.liferay.one.jira.constants;
  */
 public interface AccountConstants {
 
+	public static final String ATTRIBUTE_NAME_AI_PUBLIC_COMMENTS =
+		"AI Public Comments";
+
+	public static final String ATTRIBUTE_NAME_ARR = "ARR";
+
+	public static final String ATTRIBUTE_NAME_CODE = "Code";
+
+	public static final String ATTRIBUTE_NAME_CONTACT_EMAIL_ADDRESS =
+		"Contact Email Address";
+
+	public static final String ATTRIBUTE_NAME_CUSTOMER_CONTACTS =
+		"Customer Contacts";
+
+	public static final String ATTRIBUTE_NAME_DATA_REGION = "Data Region";
+
+	public static final String ATTRIBUTE_NAME_DESCRIPTION = "Description";
+
+	public static final String ATTRIBUTE_NAME_DXP_VERSION_CONFIRMED_DATE =
+		"DXP Version Confirmed Date";
+
+	public static final String ATTRIBUTE_NAME_EXPIRED = "Expired";
+
+	public static final String ATTRIBUTE_NAME_EXTERNAL_CREATED_AT =
+		"External Created At";
+
 	public static final String ATTRIBUTE_NAME_EXTERNAL_KEY = "External Key";
 
+	public static final String ATTRIBUTE_NAME_EXTERNAL_UPDATED_AT =
+		"External Updated At";
+
+	public static final String ATTRIBUTE_NAME_FAX_NUMBER = "Fax Number";
+
+	public static final String ATTRIBUTE_NAME_GS_OPPORTUNITY = "GS Opportunity";
+
+	public static final String ATTRIBUTE_NAME_INTERNAL = "Internal";
+
+	public static final String ATTRIBUTE_NAME_LANGUAGE = "Language";
+
+	public static final String ATTRIBUTE_NAME_NAME = "Name";
+
+	public static final String ATTRIBUTE_NAME_ORGANIZATION = "Organization";
+
+	public static final String ATTRIBUTE_NAME_PHONE_NUMBER = "Phone Number";
+
+	public static final String ATTRIBUTE_NAME_PREMIUM_SERVICE =
+		"Premium Service";
+
+	public static final String ATTRIBUTE_NAME_PROFILE_EMAIL_ADDRESS =
+		"Profile Email Address";
+
+	public static final String ATTRIBUTE_NAME_STATUS = "Status";
+
+	public static final String ATTRIBUTE_NAME_SUBORGANIZATION =
+		"Sub-Organization";
+
+	public static final String ATTRIBUTE_NAME_SUBORGANIZATION_IMPORTED =
+		"Sub-Organization-Imported";
+
+	public static final String ATTRIBUTE_NAME_SUPPORT_REGION = "Support Region";
+
+	public static final String ATTRIBUTE_NAME_TIER = "Tier";
+
+	public static final String ATTRIBUTE_NAME_WEBSITE = "Website";
+
 	public static final String OBJECT_TYPE_NAME = "Account";
+
+	public static final String SCHEMA_NAME = "One Liferay";
 
 }
--- workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/jira/converter/AccountConverter.java
+++ workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/jira/converter/AccountConverter.java
@@ -5,7 +5,17 @@
 
 package com.liferay.one.jira.converter;
 
+import com.liferay.headless.admin.user.client.custom.field.CustomField;
+import com.liferay.headless.admin.user.client.custom.field.CustomValue;
+import com.liferay.headless.admin.user.client.dto.v1_0.Account;
+import com.liferay.headless.admin.user.client.dto.v1_0.AccountBrief;
+import com.liferay.headless.admin.user.client.dto.v1_0.AccountContactInformation;
+import com.liferay.headless.admin.user.client.dto.v1_0.EmailAddress;
+import com.liferay.headless.admin.user.client.dto.v1_0.Phone;
+import com.liferay.headless.admin.user.client.dto.v1_0.WebUrl;
 import com.liferay.one.jira.constants.AccountConstants;
+import com.liferay.one.jira.model.JiraAssetObject;
+import com.liferay.portal.kernel.util.ArrayUtil;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -16,6 +26,71 @@ import org.springframework.stereotype.Component;
 @Component
 public class AccountConverter extends BaseAssetObjectConverter {
 
+	public JiraAssetObject toAssetObject(Account account) {
+		JiraAssetObject jiraAssetObject = new JiraAssetObject(
+			getAttributeIds());
+
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_EXTERNAL_KEY,
+			account.getExternalReferenceCode());
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_NAME, account.getName());
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_DESCRIPTION,
+			account.getDescription());
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_EXTERNAL_CREATED_AT,
+			formatDate(account.getDateCreated()));
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_EXTERNAL_UPDATED_AT,
+			formatDate(account.getDateModified()));
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_STATUS,
+			_status(account.getStatus()));
+
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_CODE,
+			_customFieldData(account, _CUSTOM_FIELD_NAME_ACCOUNT_CODE));
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_TIER,
+			_customFieldData(account, _CUSTOM_FIELD_NAME_ACCOUNT_TIER));
+
+		AccountContactInformation accountContactInformation =
+			account.getAccountContactInformation();
+
+		if (accountContactInformation != null) {
+			jiraAssetObject.setAttributeValue(
+				AccountConstants.ATTRIBUTE_NAME_CONTACT_EMAIL_ADDRESS,
+				_getEmailAddress(accountContactInformation));
+			jiraAssetObject.setAttributeValue(
+				AccountConstants.ATTRIBUTE_NAME_FAX_NUMBER,
+				_getFaxNumber(accountContactInformation));
+			jiraAssetObject.setAttributeValue(
+				AccountConstants.ATTRIBUTE_NAME_PHONE_NUMBER,
+				_getPhoneNumber(accountContactInformation));
+			jiraAssetObject.setAttributeValue(
+				AccountConstants.ATTRIBUTE_NAME_WEBSITE,
+				_getWebsite(accountContactInformation));
+		}
+
+		return jiraAssetObject;
+	}
+
+	public JiraAssetObject toAssetObjectFromAccountBrief(
+		AccountBrief accountBrief) {
+
+		JiraAssetObject jiraAssetObject = new JiraAssetObject(
+			getAttributeIds());
+
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_EXTERNAL_KEY,
+			accountBrief.getExternalReferenceCode());
+		jiraAssetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_NAME, accountBrief.getName());
+
+		return jiraAssetObject;
+	}
+
 	@Override
 	protected String getObjectSchemaName() {
 		return _schemaName;
@@ -25,6 +100,125 @@ public class AccountConverter extends BaseAssetObjectConverter {
 	protected String getObjectTypeName() {
 		return AccountConstants.OBJECT_TYPE_NAME;
 	}
+
+	private Object _customFieldData(Account account, String name) {
+		CustomField[] customFields = account.getCustomFields();
+
+		if (customFields == null) {
+			return null;
+		}
+
+		for (CustomField customField : customFields) {
+			if (!name.equals(customField.getName())) {
+				continue;
+			}
+
+			CustomValue customValue = customField.getCustomValue();
+
+			if (customValue == null) {
+				return null;
+			}
+
+			return customValue.getData();
+		}
+
+		return null;
+	}
+
+	private String _getEmailAddress(
+		AccountContactInformation accountContactInformation) {
+
+		EmailAddress[] emailAddresses =
+			accountContactInformation.getEmailAddresses();
+
+		if (ArrayUtil.isEmpty(emailAddresses)) {
+			return null;
+		}
+
+		for (EmailAddress emailAddress : emailAddresses) {
+			if (Boolean.TRUE.equals(emailAddress.getPrimary())) {
+				return emailAddress.getEmailAddress();
+			}
+		}
+
+		EmailAddress emailAddress = emailAddresses[0];
+
+		return emailAddress.getEmailAddress();
+	}
+
+	private String _getFaxNumber(
+		AccountContactInformation accountContactInformation) {
+
+		Phone[] telephones = ArrayUtil.filter(
+			accountContactInformation.getTelephones(),
+			telephone -> "fax".equalsIgnoreCase(telephone.getPhoneType()));
+
+		if (ArrayUtil.isEmpty(telephones)) {
+			return null;
+		}
+
+		Phone telephone = telephones[0];
+
+		return telephone.getPhoneNumber();
+	}
+
+	private String _getPhoneNumber(
+		AccountContactInformation accountContactInformation) {
+
+		Phone[] telephones = ArrayUtil.filter(
+			accountContactInformation.getTelephones(),
+			telephone -> !"fax".equalsIgnoreCase(telephone.getPhoneType()));
+
+		if (ArrayUtil.isEmpty(telephones)) {
+			return null;
+		}
+
+		for (Phone telephone : telephones) {
+			if (Boolean.TRUE.equals(telephone.getPrimary())) {
+				return telephone.getPhoneNumber();
+			}
+		}
+
+		Phone telephone = telephones[0];
+
+		return telephone.getPhoneNumber();
+	}
+
+	private String _getWebsite(
+		AccountContactInformation accountContactInformation) {
+
+		WebUrl[] webUrls = accountContactInformation.getWebUrls();
+
+		if (ArrayUtil.isEmpty(webUrls)) {
+			return null;
+		}
+
+		for (WebUrl webUrl : webUrls) {
+			if (Boolean.TRUE.equals(webUrl.getPrimary())) {
+				return webUrl.getUrl();
+			}
+		}
+
+		WebUrl webUrl = webUrls[0];
+
+		return webUrl.getUrl();
+	}
+
+	private String _status(Integer status) {
+		if (status == null) {
+			return null;
+		}
+
+		if (status == 0) {
+			return "Active";
+		}
+
+		return "Closed";
+	}
+
+	private static final String _CUSTOM_FIELD_NAME_ACCOUNT_CODE = "accountCode";
+
+	private static final String _CUSTOM_FIELD_NAME_ACCOUNT_TIER = "accountTier";
 
 	@Value("${liferay.one.jira.asset.schema.name}")
 	private String _schemaName;
