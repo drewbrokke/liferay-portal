9619caa7d8c720c8ddd0bcc4e3a20f1b045748b9
Drew Brokke <drew.brokke@liferay.com>
2026-07-02 07:52:51 -0500

    LPD-90495 WIP Rest Controllers

--- workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/AccountsRestController.java
+++ workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/AccountsRestController.java
@@ -5,9 +5,23 @@
 
 package com.liferay.one;
 
+import com.liferay.headless.admin.user.client.dto.v1_0.Account;
+import com.liferay.headless.admin.user.client.dto.v1_0.UserAccount;
+import com.liferay.one.jira.constants.AccountConstants;
+import com.liferay.one.jira.converter.AccountConverter;
+import com.liferay.one.jira.converter.ContactConverter;
+import com.liferay.one.jira.model.JiraAssetObject;
 import com.liferay.one.jira.service.AccountAssetService;
+import com.liferay.one.jira.service.AssetReferenceResolverService;
 import com.liferay.one.permission.BusinessEventPermission;
+import com.liferay.one.service.AccountService;
+import com.liferay.one.service.UserAccountService;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,8 +30,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  * @author Jenny Chen
@@ -40,10 +56,76 @@ public class AccountsRestController extends OneBaseRestController {
 			HttpStatus.OK);
 	}
 
+	@PostMapping("/sync-to-jsm")
+	public ResponseEntity<Void> postSyncToJSM() throws Exception {
+		try {
+			List<Account> accounts = _accountService.getAllAccounts();
+
+			if (_log.isInfoEnabled()) {
+				_log.info("Syncing " + accounts.size() + " accounts to JSM");
+			}
+
+			for (Account account : accounts) {
+				_syncAccount(account);
+			}
+
+			if (_log.isInfoEnabled()) {
+				_log.info("Synced " + accounts.size() + " accounts to JSM");
+			}
+		}
+		catch (Exception exception) {
+			throw new ResponseStatusException(
+				HttpStatus.INTERNAL_SERVER_ERROR,
+				"There was a problem synchronizing the JIRA object keys",
+				exception);
+		}
+
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	private void _syncAccount(Account account) throws Exception {
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				"Syncing account " + account.getExternalReferenceCode() +
+					" to JSM");
+		}
+
+		JiraAssetObject assetObject = _accountConverter.toAssetObject(account);
+
+		assetObject.setAttributeValue(
+			AccountConstants.ATTRIBUTE_NAME_CUSTOMER_CONTACTS,
+			_assetReferenceResolverService.resolveOrCreateObjectIds(
+				_contactConverter,
+				_userAccountService.getAccountUserAccounts(account.getId()),
+				UserAccount::getExternalReferenceCode,
+				_contactConverter::toAssetObject));
+
+		_accountAssetService.upsertJSMAccount(
+			account.getExternalReferenceCode(), assetObject);
+	}
+
+	private static final Log _log = LogFactory.getLog(
+		AccountsRestController.class);
+
 	@Autowired
 	private AccountAssetService _accountAssetService;
 
 	@Autowired
+	private AccountConverter _accountConverter;
+
+	@Autowired
+	private AccountService _accountService;
+
+	@Autowired
+	private AssetReferenceResolverService _assetReferenceResolverService;
+
+	@Autowired
 	private BusinessEventPermission _businessEventPermission;
+
+	@Autowired
+	private ContactConverter _contactConverter;
+
+	@Autowired
+	private UserAccountService _userAccountService;
 
 }
--- workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/UserAccountsRestController.java
+++ workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/UserAccountsRestController.java
@@ -5,15 +5,31 @@
 
 package com.liferay.one;
 
+import com.liferay.headless.admin.user.client.dto.v1_0.AccountBrief;
+import com.liferay.headless.admin.user.client.dto.v1_0.UserAccount;
+import com.liferay.one.jira.constants.ContactConstants;
+import com.liferay.one.jira.converter.AccountConverter;
+import com.liferay.one.jira.converter.ContactConverter;
+import com.liferay.one.jira.model.JiraAssetObject;
+import com.liferay.one.jira.service.AssetReferenceResolverService;
+import com.liferay.one.jira.service.ContactAssetService;
 import com.liferay.one.service.AccountService;
 import com.liferay.one.service.ProjectMembershipService;
 import com.liferay.one.service.ProvisioningEmailService;
+import com.liferay.one.service.UserAccountService;
+import com.liferay.portal.kernel.util.ListUtil;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -66,13 +82,79 @@ public class UserAccountsRestController extends OneBaseRestController {
 			userId, _accountService.fetchAccount(accountId));
 	}
 
+	@PostMapping("/sync-to-jsm")
+	public ResponseEntity<Void> postSyncToJSM() throws Exception {
+		try {
+			List<UserAccount> userAccounts =
+				_userAccountService.getAllUserAccounts();
+
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					"Syncing " + userAccounts.size() + " user accounts to JSM");
+			}
+
+			for (UserAccount userAccount : userAccounts) {
+				if (_log.isInfoEnabled()) {
+					_log.info(
+						"Syncing user account " +
+							userAccount.getExternalReferenceCode() + " to JSM");
+				}
+
+				JiraAssetObject assetObject = _contactConverter.toAssetObject(
+					userAccount);
+
+				assetObject.setAttributeValue(
+					ContactConstants.ATTRIBUTE_NAME_ACCOUNT,
+					_assetReferenceResolverService.resolveOrCreateObjectIds(
+						_accountConverter,
+						ListUtil.fromArray(userAccount.getAccountBriefs()),
+						AccountBrief::getExternalReferenceCode,
+						_accountConverter::toAssetObjectFromAccountBrief));
+
+				_contactAssetService.upsertJSMContact(
+					userAccount.getExternalReferenceCode(), assetObject);
+			}
+
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					"Synced " + userAccounts.size() + " user accounts to JSM");
+			}
+		}
+		catch (Exception exception) {
+			throw new ResponseStatusException(
+				HttpStatus.INTERNAL_SERVER_ERROR,
+				"There was a problem synchronizing user accounts to JSM",
+				exception);
+		}
+
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	private static final Log _log = LogFactory.getLog(
+		UserAccountsRestController.class);
+
+	@Autowired
+	private AccountConverter _accountConverter;
+
 	@Autowired
 	private AccountService _accountService;
+
+	@Autowired
+	private AssetReferenceResolverService _assetReferenceResolverService;
+
+	@Autowired
+	private ContactAssetService _contactAssetService;
+
+	@Autowired
+	private ContactConverter _contactConverter;
 
 	@Autowired
 	private ProjectMembershipService _projectMembershipService;
 
 	@Autowired
 	private ProvisioningEmailService _provisioningEmailService;
+
+	@Autowired
+	private UserAccountService _userAccountService;
 
 }
