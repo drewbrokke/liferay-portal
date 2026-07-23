a927b1c7bfadca03882ef825232cb17c239673b7
Drew Brokke <drew.brokke@liferay.com>
2026-07-23 16:15:17 -0500

    LPD-90495 adds Role assignment synchronizers

---
+++ workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/jira/synchronizer/OrganizationUserAccountRoleSynchronizer.java
@@ -0,0 +1,110 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.one.jira.synchronizer;
+
+import com.liferay.one.jira.constants.TeamContactRoleAssignmentConstants;
+import com.liferay.one.jira.converter.ContactConverter;
+import com.liferay.one.jira.converter.ContactRoleConverter;
+import com.liferay.one.jira.converter.TeamContactRoleAssignmentConverter;
+import com.liferay.one.jira.converter.TeamConverter;
+import com.liferay.one.jira.model.JiraAssetObject;
+import com.liferay.one.jira.service.AssetObjectUpsertService;
+import com.liferay.one.jira.service.AssetReferenceObjectService;
+import com.liferay.petra.string.StringBundler;
+
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Drew Brokke
+ */
+@Component
+public class OrganizationUserAccountRoleSynchronizer {
+
+	public void syncAssignRole(
+			String roleExternalKey, String userAccountExternalKey,
+			String organizationExternalKey)
+		throws Exception {
+
+		_syncAssignment(
+			roleExternalKey, userAccountExternalKey, organizationExternalKey,
+			false);
+	}
+
+	public void syncUnassignRole(
+			String roleExternalKey, String userAccountExternalKey,
+			String organizationExternalKey)
+		throws Exception {
+
+		_syncAssignment(
+			roleExternalKey, userAccountExternalKey, organizationExternalKey,
+			true);
+	}
+
+	private void _syncAssignment(
+			String roleExternalKey, String userAccountExternalKey,
+			String organizationExternalKey, boolean deleted)
+		throws Exception {
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				StringBundler.concat(
+					deleted ? "Unassigning" : "Assigning", " role ",
+					roleExternalKey, " for user account ",
+					userAccountExternalKey, " on organization ",
+					organizationExternalKey));
+		}
+
+		JiraAssetObject jiraAssetObject =
+			_teamContactRoleAssignmentConverter.toAssetObject(
+				roleExternalKey, userAccountExternalKey,
+				organizationExternalKey, deleted, new Date());
+
+		jiraAssetObject.setAttributeValue(
+			TeamContactRoleAssignmentConstants.ATTRIBUTE_NAME_CONTACT_ROLE,
+			_assetReferenceObjectService.getReferenceObjectId(
+				_contactRoleConverter, roleExternalKey));
+		jiraAssetObject.setAttributeValue(
+			TeamContactRoleAssignmentConstants.ATTRIBUTE_NAME_CONTACT,
+			_assetReferenceObjectService.getReferenceObjectId(
+				_contactConverter, userAccountExternalKey));
+		jiraAssetObject.setAttributeValue(
+			TeamContactRoleAssignmentConstants.ATTRIBUTE_NAME_TEAM,
+			_assetReferenceObjectService.getReferenceObjectId(
+				_teamConverter, organizationExternalKey));
+
+		_assetObjectUpsertService.upsert(
+			_teamContactRoleAssignmentConverter, jiraAssetObject);
+	}
+
+	private static final Log _log = LogFactory.getLog(
+		OrganizationUserAccountRoleSynchronizer.class);
+
+	@Autowired
+	private AssetObjectUpsertService _assetObjectUpsertService;
+
+	@Autowired
+	private AssetReferenceObjectService _assetReferenceObjectService;
+
+	@Autowired
+	private ContactConverter _contactConverter;
+
+	@Autowired
+	private ContactRoleConverter _contactRoleConverter;
+
+	@Autowired
+	private TeamContactRoleAssignmentConverter
+		_teamContactRoleAssignmentConverter;
+
+	@Autowired
+	private TeamConverter _teamConverter;
+
+}
