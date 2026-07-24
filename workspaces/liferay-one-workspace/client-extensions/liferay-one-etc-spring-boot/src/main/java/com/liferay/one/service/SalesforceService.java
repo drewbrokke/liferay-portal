437492884e27f6ff74d81174feeecf60be567606
Amos Fong <amos.fong@liferay.com>
2026-07-24 11:07:02 -0700

    LPD-98248 SF

--- workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/service/SalesforceService.java
+++ workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/main/java/com/liferay/one/service/SalesforceService.java
@@ -11,17 +11,32 @@ import com.google.auth.oauth2.IdTokenCredentials;
 import com.google.auth.oauth2.IdTokenProvider;
 
 import com.liferay.client.extension.util.spring.boot3.service.BaseService;
-import com.liferay.one.model.SalesforceOpportunity;
+import com.liferay.headless.admin.address.client.dto.v1_0.Country;
+import com.liferay.headless.admin.address.client.dto.v1_0.Region;
+import com.liferay.headless.admin.user.client.dto.v1_0.UserAccount;
+import com.liferay.headless.commerce.admin.catalog.client.custom.field.CustomField;
+import com.liferay.headless.commerce.admin.catalog.client.custom.field.CustomValue;
+import com.liferay.headless.commerce.admin.catalog.client.dto.v1_0.Sku;
+import com.liferay.headless.commerce.admin.order.client.dto.v1_0.Account;
+import com.liferay.headless.commerce.admin.order.client.dto.v1_0.BillingAddress;
+import com.liferay.headless.commerce.admin.order.client.dto.v1_0.Order;
+import com.liferay.headless.commerce.admin.order.client.dto.v1_0.OrderItem;
+import com.liferay.one.util.CommerceOrderUtil;
 import com.liferay.petra.string.StringBundler;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
 import java.util.Date;
+import java.util.Objects;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -36,12 +51,17 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class SalesforceService extends BaseService {
 
 	public JSONObject postSalesforceOpportunity(
-			SalesforceOpportunity salesforceOpportunity)
+			Country country, String licenseType, Order order, Sku sku,
+			UserAccount userAccount)
 		throws Exception {
+
+		JSONObject salesforceOpportunityJSONObject =
+			_getSalesforceOpportunityJSONObject(
+				country, licenseType, order, sku, userAccount);
 
 		try {
 			String response = post(
-				_getAuthorization(), salesforceOpportunity.toString(),
+				_getAuthorization(), salesforceOpportunityJSONObject.toString(),
 				UriComponentsBuilder.fromUriString(
 					_gcfBaseUrl
 				).path(
@@ -51,7 +71,8 @@ public class SalesforceService extends BaseService {
 
 			if (_log.isInfoEnabled()) {
 				_log.info(
-					"Created Salesforce opportunity " + salesforceOpportunity);
+					"Created Salesforce opportunity " +
+						salesforceOpportunityJSONObject);
 			}
 
 			return new JSONObject(response);
@@ -60,18 +81,40 @@ public class SalesforceService extends BaseService {
 			_log.error(
 				StringBundler.concat(
 					"Unable to create Salesforce opportunity ",
-					salesforceOpportunity, ":\n",
+					salesforceOpportunityJSONObject, ":\n",
 					webClientResponseException.getResponseBodyAsString()));
 
 			return null;
 		}
 	}
 
+	private String _format(Date date) {
+		if (date == null) {
+			return null;
+		}
+
+		return date.toInstant(
+		).atZone(
+			ZoneOffset.UTC
+		).toLocalDate(
+		).format(
+			DateTimeFormatter.ISO_LOCAL_DATE
+		);
+	}
+
+	private String _getAccountId(Order order) {
+		Account account = order.getAccount();
+
+		return account.getExternalReferenceCode();
+	}
+
 	private String _getAuthorization() throws Exception {
 		if (_accessToken != null) {
 			Date expirationTime = _accessToken.getExpirationTime();
 
-			if (System.currentTimeMillis() < expirationTime.getTime()) {
+			if ((System.currentTimeMillis() + _EXPIRATION_BUFFER) <
+					expirationTime.getTime()) {
+
 				return _authorization;
 			}
 		}
@@ -100,6 +143,256 @@ public class SalesforceService extends BaseService {
 			return _authorization;
 		}
 	}
+
+	private JSONObject _getBillingAddressJSONObject(
+		Country country, Order order) {
+
+		BillingAddress billingAddress = order.getBillingAddress();
+
+		return new JSONObject(
+		).put(
+			"addressName", billingAddress.getName()
+		).put(
+			"city", billingAddress.getCity()
+		).put(
+			"country",
+			CommerceOrderUtil.getDefaultLocale(country.getTitle_i18n())
+		).put(
+			"postalCode", billingAddress.getZip()
+		).put(
+			"state", _getState(country, order)
+		).put(
+			"street",
+			billingAddress.getStreet1() + " " + billingAddress.getStreet2()
+		);
+	}
+
+	private JSONArray _getLineItemsJSONArray(
+		String licenseType, Order order, Sku sku) {
+
+		String productId = null;
+
+		for (CustomField customField : sku.getCustomFields()) {
+			if (Objects.equals(
+					customField.getName(), "salesforce-product-id")) {
+
+				CustomValue customValue = customField.getCustomValue();
+
+				Object data = customValue.getData();
+
+				if (data != null) {
+					productId = data.toString();
+				}
+
+				break;
+			}
+		}
+
+		JSONArray jsonArray = new JSONArray();
+
+		for (OrderItem orderItem : order.getOrderItems()) {
+			JSONObject jsonObject = new JSONObject();
+
+			if (!Objects.equals(
+					order.getOrderTypeExternalReferenceCode(),
+					"AI_HUB_TOKEN")) {
+
+				jsonObject.put(
+					"endDate",
+					_format(
+						CommerceOrderUtil.getOrderPurchaseEndDate(
+							licenseType,
+							CommerceOrderUtil.getSkuOptionValue(
+								"license-usage-type", orderItem.getOptions())))
+				).put(
+					"startDate", _format(order.getCreateDate())
+				);
+			}
+
+			jsonArray.put(
+				jsonObject.put(
+					"orderType", "New"
+				).put(
+					"productId", productId
+				).put(
+					"quantity", orderItem.getQuantity()
+				).put(
+					"unitPrice", orderItem.getUnitPrice()
+				));
+		}
+
+		return jsonArray;
+	}
+
+	private String _getPaymentMethodType(Order order) {
+		if (Objects.equals(order.getPaymentMethod(), "money-order")) {
+			return "Offline";
+		}
+
+		return "Online";
+	}
+
+	private String _getPrimaryContactEmailAddress(Order order) {
+		JSONObject orderMetadataJSONObject =
+			CommerceOrderUtil.getOrderMetadataJSONObject(order);
+
+		if (Objects.equals(
+				order.getOrderTypeExternalReferenceCode(), "AI_HUB")) {
+
+			JSONObject aiHubFormJSONObject =
+				orderMetadataJSONObject.optJSONObject(
+					"aiHubForm", new JSONObject());
+
+			return aiHubFormJSONObject.optString(
+				"administratorEmailAddress", order.getCreatorEmailAddress());
+		}
+
+		JSONObject provisioningFormJSONObject =
+			orderMetadataJSONObject.optJSONObject(
+				"provisioningForm", new JSONObject());
+
+		return provisioningFormJSONObject.optString(
+			"ownerEmailAddress", order.getCreatorEmailAddress());
+	}
+
+	private JSONObject _getPrimaryContactJSONObject(
+		Order order, UserAccount userAccount) {
+
+		return new JSONObject(
+		).put(
+			"email", _getPrimaryContactEmailAddress(order)
+		).put(
+			"firstName", userAccount.getGivenName()
+		).put(
+			"lastName", userAccount.getFamilyName()
+		).put(
+			"role", "Opportunity Owner"
+		);
+	}
+
+	private JSONObject _getProjectJSONObject(
+		Order order, UserAccount userAccount) {
+
+		JSONObject orderMetadataJSONObject =
+			CommerceOrderUtil.getOrderMetadataJSONObject(order);
+
+		JSONObject projectJSONObject = new JSONObject(
+		).put(
+			"projectId",
+			orderMetadataJSONObject.getString("salesforceProjectId")
+		);
+
+		if (Objects.equals(
+				order.getOrderTypeExternalReferenceCode(), "AI_HUB")) {
+
+			JSONObject aiHubFormJSONObject =
+				orderMetadataJSONObject.getJSONObject("aiHubForm");
+
+			return projectJSONObject.put(
+				"aiHubAccountName",
+				aiHubFormJSONObject.optString("aiHubAccountName")
+			).put(
+				"projectContacts",
+				new JSONArray(
+				).put(
+					_getPrimaryContactJSONObject(
+						order, userAccount
+					).put(
+						"role", "AI Hub Administrator"
+					)
+				)
+			);
+		}
+
+		if (Objects.equals(
+				order.getOrderTypeExternalReferenceCode(), "AI_HUB_TOKEN")) {
+
+			return projectJSONObject.put("projectContacts", new JSONArray());
+		}
+
+		JSONObject provisioningFormJSONObject =
+			orderMetadataJSONObject.optJSONObject("provisioningForm");
+
+		if (provisioningFormJSONObject == null) {
+			return projectJSONObject;
+		}
+
+		return projectJSONObject.put(
+			"allowedEmailDomains",
+			provisioningFormJSONObject.optString("allowedEmailDomains")
+		).put(
+			"dataCenterLocation",
+			provisioningFormJSONObject.optString("dataCenterLocation")
+		).put(
+			"friendlyWorkspaceURL",
+			provisioningFormJSONObject.optString("friendlyWorkspaceURL")
+		).put(
+			"projectContacts",
+			new JSONArray(
+			).put(
+				_getPrimaryContactJSONObject(
+					order, userAccount
+				).put(
+					"role", "LDP Administrator"
+				)
+			)
+		).put(
+			"securityContactEmailAddress", order.getCreatorEmailAddress()
+		).put(
+			"workspaceName",
+			provisioningFormJSONObject.optString("workspaceName")
+		);
+	}
+
+	private JSONObject _getSalesforceOpportunityJSONObject(
+		Country country, String licenseType, Order order, Sku sku,
+		UserAccount userAccount) {
+
+		return new JSONObject(
+		).put(
+			"accountId", _getAccountId(order)
+		).put(
+			"billingAddress", _getBillingAddressJSONObject(country, order)
+		).put(
+			"closeDate", _format(order.getCreateDate())
+		).put(
+			"lineItems", _getLineItemsJSONArray(licenseType, order, sku)
+		).put(
+			"marketplaceDealId", order.getId()
+		).put(
+			"opportunityCurrency", order.getCurrencyCode()
+		).put(
+			"opportunityOwner", "Marketplace Integration"
+		).put(
+			"paymentMethodType", _getPaymentMethodType(order)
+		).put(
+			"primaryContact", _getPrimaryContactJSONObject(order, userAccount)
+		).put(
+			"project", _getProjectJSONObject(order, userAccount)
+		).put(
+			"termType", "Single Year"
+		).put(
+			"typeOfBusiness", "Existing Business"
+		);
+	}
+
+	private String _getState(Country country, Order order) {
+		BillingAddress billingAddress = order.getBillingAddress();
+
+		for (Region region : country.getRegions()) {
+			if (Objects.equals(
+					billingAddress.getRegionISOCode(),
+					region.getRegionCode())) {
+
+				return CommerceOrderUtil.getDefaultLocale(
+					region.getTitle_i18n());
+			}
+		}
+
+		return null;
+	}
+
+	private static final long _EXPIRATION_BUFFER = 60 * 1000;
 
 	private static final Log _log = LogFactory.getLog(SalesforceService.class);
 
