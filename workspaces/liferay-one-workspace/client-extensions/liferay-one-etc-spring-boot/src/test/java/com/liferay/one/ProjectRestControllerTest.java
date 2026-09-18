35bb4f55401d20f9132829dd5dc267f5ddd4cf90
Drew Brokke <drew.brokke@liferay.com>
2026-09-18 14:37:41 -0500

    LPD-99837 calculate bucket size from the UsageDefinition rather than the constant

--- workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/test/java/com/liferay/one/ProjectRestControllerTest.java
+++ workspaces/liferay-one-workspace/client-extensions/liferay-one-etc-spring-boot/src/test/java/com/liferay/one/ProjectRestControllerTest.java
@@ -8,6 +8,7 @@ package com.liferay.one;
 import com.liferay.headless.admin.user.client.dto.v1_0.Account;
 import com.liferay.headless.admin.user.client.dto.v1_0.UserAccount;
 import com.liferay.one.constants.CommerceProductConstants;
+import com.liferay.one.constants.EntitlementConstants;
 import com.liferay.one.constants.PropertyConstants;
 import com.liferay.one.exception.GoogleCloudFunctionUnavailableException;
 import com.liferay.one.exception.InvalidUsageParameterException;
@@ -18,19 +19,23 @@ import com.liferay.one.jira.synchronizer.AccountUserAccountRoleSynchronizer;
 import com.liferay.one.jira.synchronizer.UserAccountSynchronizer;
 import com.liferay.one.model.BaseUsageStrategy;
 import com.liferay.one.model.Entitlement;
+import com.liferay.one.model.EntitlementDefinition;
 import com.liferay.one.model.ExperienceUsageStrategy;
 import com.liferay.one.model.LDPEventUsageStrategy;
 import com.liferay.one.model.LDPUsageStrategy;
 import com.liferay.one.model.Project;
+import com.liferay.one.model.UsageDefinition;
 import com.liferay.one.permission.BusinessEventPermission;
 import com.liferay.one.service.AccountService;
 import com.liferay.one.service.CommerceProductService;
 import com.liferay.one.service.CommerceSkuService;
+import com.liferay.one.service.EntitlementDefinitionService;
 import com.liferay.one.service.EntitlementService;
 import com.liferay.one.service.GoogleCloudFunctionService;
 import com.liferay.one.service.ProjectMembershipService;
 import com.liferay.one.service.ProjectService;
 import com.liferay.one.service.PropertyService;
+import com.liferay.one.service.UsageDefinitionService;
 import com.liferay.one.service.UserAccountService;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
@@ -80,6 +85,9 @@ public class ProjectRestControllerTest {
 		ReflectionTestUtils.setField(
 			_projectRestController, "_commerceSkuService", _commerceSkuService);
 		ReflectionTestUtils.setField(
+			_projectRestController, "_entitlementDefinitionService",
+			_entitlementDefinitionService);
+		ReflectionTestUtils.setField(
 			_projectRestController, "_entitlementService", _entitlementService);
 		ReflectionTestUtils.setField(
 			_projectRestController, "_googleCloudFunctionService",
@@ -92,15 +100,44 @@ public class ProjectRestControllerTest {
 		ReflectionTestUtils.setField(
 			_projectRestController, "_propertyService", _propertyService);
 		ReflectionTestUtils.setField(
+			_projectRestController, "_usageDefinitionService",
+			_usageDefinitionService);
+		ReflectionTestUtils.setField(
 			_projectRestController, "_userAccountService", _userAccountService);
 		ReflectionTestUtils.setField(
 			_projectRestController, "_userAccountSynchronizer",
 			_userAccountSynchronizer);
 
+		EntitlementDefinition entitlementDefinition = new EntitlementDefinition(
+			new JSONObject(
+			).put(
+				"id", 3L
+			));
+
+		Mockito.when(
+			_entitlementDefinitionService.fetchEntitlementDefinition(
+				EntitlementConstants.
+					EXTERNAL_REFERENCE_CODE_DATA_PLATFORM_EVENTS_ADD_ON_BUCKET)
+		).thenReturn(
+			entitlementDefinition
+		);
+
 		Mockito.when(
 			_projectService.fetchProject(_PROJECT_EXTERNAL_REFERENCE_CODE)
 		).thenReturn(
 			_createProject()
+		);
+
+		Mockito.when(
+			_usageDefinitionService.fetchUsageDefinition(entitlementDefinition)
+		).thenReturn(
+			new UsageDefinition(
+				new JSONObject(
+				).put(
+					"id", 4L
+				).put(
+					"overageBucketSize", 200000
+				))
 		);
 
 		_setUpProductName(_PRODUCT_NAME_EXPERIENCE);
@@ -1568,6 +1605,8 @@ public class ProjectRestControllerTest {
 		CommerceProductService.class);
 	private final CommerceSkuService _commerceSkuService = Mockito.mock(
 		CommerceSkuService.class);
+	private final EntitlementDefinitionService _entitlementDefinitionService =
+		Mockito.mock(EntitlementDefinitionService.class);
 	private final EntitlementService _entitlementService = Mockito.mock(
 		EntitlementService.class);
 	private final GoogleCloudFunctionService _googleCloudFunctionService =
@@ -1579,6 +1618,8 @@ public class ProjectRestControllerTest {
 		ProjectService.class);
 	private final PropertyService _propertyService = Mockito.mock(
 		PropertyService.class);
+	private final UsageDefinitionService _usageDefinitionService = Mockito.mock(
+		UsageDefinitionService.class);
 	private final UserAccountService _userAccountService = Mockito.mock(
 		UserAccountService.class);
 	private final UserAccountSynchronizer _userAccountSynchronizer =
